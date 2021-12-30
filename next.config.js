dee1aa4ce55046489c425211ddd405a4e998f7f6
Michael Snook <me@michaelsnook.com>
2021-12-30 15:52:00 +0530

    Fix up content provider and hook
    Now the content hook may run many times but the network tab
    shows that Profile data is only being fetched once or twice
    even as you navigate around the app, so basically the job
    is done. It still seems like the sidebar is rerendering
    too much but as long as it's no re-fetching the data it's
    acceptable for now.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['next.sunlo.co', 'efxtdmrwlbrsjamsierr.supabase.co'],
+  },
 }
--- pages/app/profile/index.js
+++ pages/app/profile/index.js
@@ -8,65 +8,68 @@ export async function sendPasswordResetEmail() {
   return true
 }
 
-const ProfileCard = ({ profile, user }) => (
-  <div className="card shadow-xl p-6 my-4 bordered grid grid-cols-1 sm:grid-cols-2 gap-4">
-    <div className="flex flex-col">
-      <label className="font-bold px-3">name</label>
-      <input
-        type="text"
-        className="border rounded p-3"
-        value={profile?.username}
-        disabled
-      />
+const ProfileCard = () => {
+  const { user, profile } = useGlobalState()
+  return !user || !profile ? null : (
+    <div className="card shadow-xl p-6 my-4 bordered grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className="flex flex-col">
+        <label className="font-bold px-3">name</label>
+        <input
+          type="text"
+          className="border rounded p-3"
+          value={profile?.username}
+          disabled
+        />
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold px-3">email</label>
+        <input
+          type="text"
+          className="border rounded p-3"
+          value={user?.email}
+          disabled
+        />
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold px-3">languages you speak</label>
+        <input
+          type="text"
+          className="border rounded p-3"
+          value={profile?.languages_spoken}
+          disabled
+        />
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold px-3">your primary language</label>
+        <input
+          type="text"
+          className="border rounded p-3"
+          value={profile?.language_primary}
+          disabled
+        />
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold px-3">reset your password</label>
+        <button
+          className="btn btn-quiet"
+          onClick={sendPasswordResetEmail}
+          role="button"
+        >
+          Send password reset email
+        </button>
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold px-3">your user ID</label>
+        <input
+          type="text"
+          className="border rounded p-3"
+          value={user?.id}
+          disabled
+        />
+      </div>
     </div>
-    <div className="flex flex-col">
-      <label className="font-bold px-3">email</label>
-      <input
-        type="text"
-        className="border rounded p-3"
-        value={user?.email}
-        disabled
-      />
-    </div>
-    <div className="flex flex-col">
-      <label className="font-bold px-3">languages you speak</label>
-      <input
-        type="text"
-        className="border rounded p-3"
-        value={profile?.languages_spoken}
-        disabled
-      />
-    </div>
-    <div className="flex flex-col">
-      <label className="font-bold px-3">your primary language</label>
-      <input
-        type="text"
-        className="border rounded p-3"
-        value={profile?.language_primary}
-        disabled
-      />
-    </div>
-    <div className="flex flex-col">
-      <label className="font-bold px-3">reset your password</label>
-      <button
-        className="btn btn-quiet"
-        onClick={sendPasswordResetEmail}
-        role="button"
-      >
-        Send password reset email
-      </button>
-    </div>
-    <div className="flex flex-col">
-      <label className="font-bold px-3">your user ID</label>
-      <input
-        type="text"
-        className="border rounded p-3"
-        value={user?.id}
-        disabled
-      />
-    </div>
-  </div>
-)
+  )
+}
 
 export default function Profile() {
   const { user, profile, profileError: error } = useGlobalState()
@@ -81,21 +84,21 @@ export default function Profile() {
           htmlFor="single"
         >
           <Image
-            src={`/images/avatars/${profile?.avatar_url}`}
+            src={profile?.avatar_public_url}
             width="144"
             height="144"
-            alt={profile?.data?.username}
+            alt={`${profile?.username}'s profile image`}
           />
         </label>
       </div>
       <div>
-        <h2 className="text-4xl mb-1">Hi, {profile?.data?.username} 👋</h2>
+        <h2 className="text-4xl mb-1">Hi, {profile?.username} 👋</h2>
         <span className="inline-block px-3 py-2 bg-gray-400 text-white rounded-full">
           You&apos;re working on {profile?.decks?.length || 'zero'} languages
           right now.
         </span>
       </div>
-      <ProfileCard profile={profile} user={user} />
+      <ProfileCard />
     </AppLayout>
   )
 }
