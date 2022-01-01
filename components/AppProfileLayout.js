104e7e0d28731604d828f6ce3582e81c31496bd9
Michael Snook <me@michaelsnook.com>
2022-01-01 17:44:47 +0530

    Wire up the Profile page to allow user updates

---
+++ components/AppProfileLayout.js
@@ -0,0 +1,44 @@
+import Image from 'next/image'
+import AppLayout from './AppLayout'
+import { useGlobalState } from '../lib/global-store'
+
+const AvatarSection = () => {
+  const { profile } = useGlobalState()
+  return (
+    <>
+      <div className="avatar relative">
+        <label
+          className="mb-8 w-36 h-36 mask mask-circle shadow-lg bg-gray-200"
+          htmlFor="single"
+        >
+          {profile?.avatar_public_url ? (
+            <Image
+              src={profile?.avatar_public_url}
+              width="144"
+              height="144"
+              alt={`${profile?.username}'s profile image`}
+            />
+          ) : null}
+        </label>
+      </div>
+      <div>
+        <h2 className="text-4xl mb-1">Hi, {profile?.username} 👋</h2>
+        <span className="inline-block px-3 py-2 bg-gray-400 text-white rounded-full">
+          You&apos;re working on {profile?.decks?.length || 'zero'} languages
+          right now.
+        </span>
+      </div>
+    </>
+  )
+}
+
+export default function AppProfileLayout({ children }) {
+  return (
+    <AppLayout>
+      <>
+        <AvatarSection />
+        {children}
+      </>
+    </AppLayout>
+  )
+}
--- pages/app/profile/index.js
+++ pages/app/profile/index.js
@@ -1,111 +1,129 @@
 // import Link from 'next/link'
-import Image from 'next/image'
-import AppLayout from '../../../components/AppLayout'
+import { useState } from 'react'
+import Link from 'next/link'
+import AppProfileLayout from '../../../components/AppProfileLayout'
+import supabase from '../../../lib/supabase-client'
 import { useGlobalState } from '../../../lib/global-store'
-
-export async function sendPasswordResetEmail() {
-  // const { data, error } = supabase.auth.sendPassword whatever whatever
-  return true
-}
+import ErrorList from '../../../components/ErrorList'
 
 const ProfileCard = () => {
-  const { user, profile } = useGlobalState()
-  return !user || !profile ? null : (
-    <div className="card shadow-xl p-6 my-4 bordered grid grid-cols-1 sm:grid-cols-2 gap-4">
-      <div className="flex flex-col">
-        <label className="font-bold px-3">name</label>
-        <input
-          type="text"
-          className="border rounded p-3"
-          value={profile?.username}
-          disabled
-        />
+  const { user, profile, setProfile } = useGlobalState()
+  const [errors, setErrors] = useState()
+  const [isSubmitting, setIsSubmitting] = useState()
+  const onSubmit = event => {
+    event.preventDefault()
+    setErrors()
+    setIsSubmitting(false)
+    console.log(event.target)
+    const username = event.target.username.value
+    const language_primary = event.target.language_primary.value
+    const languages_spoken = event.target.languages_spoken.value
+    supabase
+      .from('profile')
+      .update({ username, language_primary, languages_spoken })
+      .match({ id: profile.id })
+      .then(({ data, error }) => {
+        setIsSubmitting(false)
+        if (error) {
+          setErrors(error)
+          console.log('error', error)
+        } else {
+          // merge the objects so we keep avatar_public_url
+          setProfile({ ...profile, ...data[0] })
+          console.log('data', data)
+        }
+      })
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <fieldset
+        className="card shadow-xl p-6 my-8 border grid grid-cols-1 sm:grid-cols-2 gap-4"
+        disabled={!profile || isSubmitting}
+      >
+        <div className="flex flex-col">
+          <label className="font-bold px-3">Your nickname</label>
+          <input
+            type="text"
+            className="border rounded p-3"
+            name="username"
+            tabIndex="1"
+            defaultValue={profile?.username || ''}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="font-bold px-3">Languages you know</label>
+          <input
+            type="text"
+            className="border rounded p-3"
+            name="languages_spoken"
+            defaultValue={profile ? `{${profile?.languages_spoken}}` : ''}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="font-bold px-3">Primary language</label>
+          <input
+            type="text"
+            className="border rounded p-3"
+            name="language_primary"
+            defaultValue={profile?.language_primary || ''}
+          />
+        </div>
+        <div className="flex flex-col-reverse">
+          <button
+            className="btn btn-primary"
+            disabled={!profile || isSubmitting}
+          >
+            Save changes
+          </button>
+        </div>
+        <ErrorList summary="Problem updating profile" error={errors?.message} />
+      </fieldset>
+    </form>
+  )
+}
+
+const UserAuthCard = () => {
+  const { user } = useGlobalState()
+  return (
+    <div className="card shadow-xl p-6 my-8 border flex flex-col space-y-4">
+      <div className="flex flex-col w-full">
+        <label className="font-bold px-3">Your email</label>
+        <div className="flex flex-row">
+          <input
+            type="text"
+            className="border rounded p-3 flex-grow"
+            value={user?.email}
+            disabled
+          />
+          <Link href="/app/profile/change-password">
+            <a className="btn btn-quiet">Change</a>
+          </Link>
+        </div>
       </div>
       <div className="flex flex-col">
-        <label className="font-bold px-3">email</label>
-        <input
-          type="text"
-          className="border rounded p-3"
-          value={user?.email}
-          disabled
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="font-bold px-3">languages you speak</label>
-        <input
-          type="text"
-          className="border rounded p-3"
-          value={profile?.languages_spoken}
-          disabled
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="font-bold px-3">your primary language</label>
-        <input
-          type="text"
-          className="border rounded p-3"
-          value={profile?.language_primary}
-          disabled
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="font-bold px-3">reset your password</label>
-        <button
-          className="btn btn-quiet"
-          onClick={sendPasswordResetEmail}
-          role="button"
-        >
-          Send password reset email
-        </button>
-      </div>
-      <div className="flex flex-col">
-        <label className="font-bold px-3">your user ID</label>
-        <input
-          type="text"
-          className="border rounded p-3"
-          value={user?.id}
-          disabled
-        />
+        <label className="font-bold px-3">Your password</label>
+        <div className="flex flex-row">
+          <input
+            type="text"
+            className="border rounded p-3 flex-grow"
+            value="***************"
+            disabled
+          />
+          <Link href="/app/profile/change-password">
+            <a className="btn btn-quiet">Change</a>
+          </Link>
+        </div>
       </div>
     </div>
   )
 }
 
-const AvatarSection = () => {
-  const { user, profile } = useGlobalState()
-  return (
-    <>
-      <div className="avatar relative">
-        <label
-          className="mb-8 w-36 h-36 mask mask-circle shadow-lg bg-gray-200"
-          htmlFor="single"
-        >
-          {profile?.avatar_public_url ? (
-            <Image
-              src={profile?.avatar_public_url}
-              width="144"
-              height="144"
-              alt={`${profile?.username}'s profile image`}
-            />
-          ) : null}
-        </label>
-      </div>
-      <div>
-        <h2 className="text-4xl mb-1">Hi, {profile?.username} 👋</h2>
-        <span className="inline-block px-3 py-2 bg-gray-400 text-white rounded-full">
-          You&apos;re working on {profile?.decks?.length || 'zero'} languages
-          right now.
-        </span>
-      </div>
-    </>
-  )
-}
-
 export default function Profile() {
   return (
-    <AppLayout>
-      <AvatarSection />
+    <AppProfileLayout>
       <ProfileCard />
-    </AppLayout>
+      <UserAuthCard />
+    </AppProfileLayout>
   )
 }
