701165cb5325e822ee2af89b0c433d4ffa090bb9
Michael Snook <me@michaelsnook.com>
2022-01-25 15:08:34 +0530

    DRY functions into lib/deck.js

--- pages/languages/[code]/index.js
+++ pages/languages/[code]/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import supabase from 'lib/supabase-client'
 import AppLayout from 'components/AppLayout'
 import PhraseCardSmall from 'components/PhraseCardSmall'
+import { getLanguagePhrases } from 'lib/deck'
 
 export default function LanguagePage({ language, phrases }) {
   console.log(language)
@@ -21,7 +22,7 @@ export default function LanguagePage({ language, phrases }) {
           {phrases?.map(phrase => (
             <li key={`phrase-${phrase.id}`}>
               <Link href={`/phrases/${phrase.id}`}>
-                <a className="card shadow p-4 hover:bg-primary hover:text-white mb-4">
+                <a className="card bg-white text-gray-700 shadow p-4 hover:bg-[#efe9fb] mb-4">
                   <PhraseCardSmall {...phrase} />
                 </a>
               </Link>
@@ -34,19 +35,8 @@ export default function LanguagePage({ language, phrases }) {
 }
 
 export const getStaticProps = async ({ params }) => {
-  const { data: language } = await supabase
-    .from('language')
-    .select('*')
-    .eq('code', params.code)
-    .single()
-
-  const { data: phrases } = await supabase
-    .from('card_phrase')
-    .select('*, translations:card_translation(*)')
-    .eq('lang', params.code)
-
-  if (!language || !phrases) throw `Problem loading language [${params.code}]`
-  return { props: { language, phrases } }
+  const { code } = params
+  return await getLanguagePhrases(code)
 }
 
 export const getStaticPaths = async () => {
