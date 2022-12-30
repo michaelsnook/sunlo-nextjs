7741869e6661b64afd26d82f7c7fd2f3b9cc1bb4
Michael Snook <michael.snook@gmail.com>
2022-12-30 23:36:50 +0530

    Move phrase page into /app directory

---
+++ app/phrases/layout.js
@@ -0,0 +1,11 @@
+import { Providers } from 'lib/providers'
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
