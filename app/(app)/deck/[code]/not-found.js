0d5911bca0c415aefaea0ee38baaebcb5f1f8066
Michael Snook <michael.snook@gmail.com>
2023-02-25 22:21:43 +0530

    Add urql client provider for useQuery

---
+++ app/(app)/deck/[code]/not-found.js
@@ -0,0 +1,11 @@
+export default function deckNotFound() {
+  return (
+    <div className="page-card">
+      <h1 className="h1">404 - Language not found</h1>
+      <p>
+        It doesn&apos;t look like we support that language yet. Or maybe
+        there&apos;s a typo in the link?
+      </p>
+    </div>
+  )
+}
