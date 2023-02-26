52c12ec0d05c9545694ff815463fc322ec440e86
Michael Snook <me@michaelsnook.com>
2023-02-26 16:55:42 +0530

    Handle errors a little better

--- components/ErrorList.js
+++ components/ErrorList.js
@@ -1,12 +1,16 @@
-const ErrorList = ({ summary, error, errors, asCard }) =>
-  !error && !errors?.length ? null : (
+const ErrorList = ({ summary, error, errors, asCard }) => {
+  // convert neively-passed error objects to strings
+  const errorString = typeof error === 'object' ? JSON.stringify(error) : error
+
+  return !error && !errors?.length ? null : (
     <div className={`${asCard ? 'big-card' : ''} mt-6 mb-2`}>
       {summary ? <p className="font-bold text-red-600">{summary}</p> : null}
       <ul className="pl-5 text-red-600 list-disc">
-        {error ? <li key={error}>{error}</li> : null}
+        {errorString ? <li key={errorString}>{errorString}</li> : null}
         {errors ? errors.map(m => <li key={m}>{m}</li>) : null}
       </ul>
     </div>
   )
+}
 
 export default ErrorList
