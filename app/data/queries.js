83c00802f107d782bbc5ce1a3b088452c7fded9d
Michael Snook <me@michaelsnook.com>
2023-03-05 18:10:20 +0530

    Alphabetize functions in the /data folder
    Alternatively, I considered putting query-fetcher-hook into
    their own folders or even a single file for each of the types
    of data access. But some fetchers use the same queries and
    I don't feel like figuring out how to manage that, so for now,
    each of the hooks.js, fetchers.js, and queries.js files will
    just grow uncomfortably long until I either decide what to do
    about it or perhaps just get used to it, alphabetized, and
    with a pretty consistent naming pattern.
    
    Access a type, such as "DogPark" with a query dogParkQuery,
    fetcher getDogPark, and hook useDogPark. When fetching all of
    something say AllDogParks and when specifying only returning
    one of something, make sure the hook and fetcher say
    "OneDogPark", or say "DogParkDetails" when going deep into
    connected data points.

--- app/data/queries.js
+++ app/data/queries.js
@@ -1,5 +1,84 @@
 import { gql } from 'graphql-request'
 
+// gets all decks for auth'd user, and
+// all the card IDs and statuses in those decks
+// which serve as metadata for the deck
+export const allDecksQuery = gql`
+  query AllDecksQuery {
+    userDeckCollection {
+      edges {
+        node {
+          id
+          lang
+          deckMembershipCollection {
+            edges {
+              node {
+                id
+                status
+                cardPhraseId
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+// gets a big long list of cards' details; irrespective of
+// what decks or languages those cards belong to.
+// hopefully the react-query cache will
+export const allPhraseDetailsQuery = gql`
+  query AllPhraseDetailsQuery {
+    cardPhraseCollection {
+      edges {
+        node {
+          id
+          text
+          lang
+          cardTranslationCollection {
+            edges {
+              node {
+                id
+                text
+                lang
+              }
+            }
+          }
+          cardSeeAlsoCollection {
+            edges {
+              node {
+                fromPhrase {
+                  id
+                  text
+                  lang
+                }
+                toPhrase {
+                  id
+                  text
+                  lang
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export const allPhraseIDsQuery = gql`
+  query AllPhraseIDsQuery {
+    cardPhraseCollection {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+`
+
 export const deckQuery = gql`
   query DeckQuery($filter: UserDeckFilter) {
     userDeckCollection(filter: $filter) {
@@ -100,85 +179,6 @@ export const phraseDetailsQuery = gql`
               }
             }
           }
-        }
-      }
-    }
-  }
-`
-
-// gets a big long list of cards' details; irrespective of
-// what decks or languages those cards belong to.
-// hopefully the react-query cache will
-export const allPhraseDetailsQuery = gql`
-  query AllPhraseDetailsQuery {
-    cardPhraseCollection {
-      edges {
-        node {
-          id
-          text
-          lang
-          cardTranslationCollection {
-            edges {
-              node {
-                id
-                text
-                lang
-              }
-            }
-          }
-          cardSeeAlsoCollection {
-            edges {
-              node {
-                fromPhrase {
-                  id
-                  text
-                  lang
-                }
-                toPhrase {
-                  id
-                  text
-                  lang
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
-// gets all decks for auth'd user, and
-// all the card IDs and statuses in those decks
-// which serve as metadata for the deck
-export const allDecksQuery = gql`
-  query AllDecksQuery {
-    userDeckCollection {
-      edges {
-        node {
-          id
-          lang
-          deckMembershipCollection {
-            edges {
-              node {
-                id
-                status
-                cardPhraseId
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
-export const allPhraseIDsQuery = gql`
-  query AllPhraseIDsQuery {
-    cardPhraseCollection {
-      edges {
-        node {
-          id
         }
       }
     }
