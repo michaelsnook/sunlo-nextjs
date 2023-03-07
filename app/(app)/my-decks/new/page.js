5eae27a2f8391f50c9f8f0233d945e9b143ecde8
Michael Snook <me@michaelsnook.com>
2023-03-08 02:19:18 +0530

    Make it a little nicer with validation/error

--- app/(app)/my-decks/new/page.js
+++ app/(app)/my-decks/new/page.js
@@ -6,27 +6,36 @@ import Select from 'react-select'
 import languages from 'lib/languages'
 import { useAllDecks } from 'app/data/hooks'
 import ErrorList from 'components/ErrorList'
-import Loading from 'app/loading'
 
 const options = Object.keys(languages).map(code => {
   return { value: code, label: languages[code] }
 })
 
+function TinyError({ text }) {
+  return <p className="my-4 text-red-700">{text}</p>
+}
+
 export default function Page() {
   const [value, setValue] = useState()
+  const isDisabled = !(value?.length === 3)
+  const [formError, setFormError] = useState()
+
   const handleSubmit = event => {
     event.preventDefault()
+    if (!value) {
+      setFormError(`Please select a language to start learning...`)
+      console.log(`detected aform error`)
+      return
+    }
     console.log(`submit the form`, event, value)
   }
+  const handleChange = e => {
+    setFormError('')
+    setValue(e.value)
+  }
+
   const { data, error, status } = useAllDecks()
-  const liveOptions =
-    status === 'loading'
-      ? options
-      : options.map(option => {
-          return {
-            ...option,
-          }
-        })
+
   return (
     <>
       <Link href="/my-decks" className="hover:underline">
@@ -35,25 +44,34 @@ export default function Page() {
       <h1 className="h1">Start a new deck</h1>
       <div className="page-card">
         <form name="new-deck" onSubmit={handleSubmit}>
-          {status === 'loading' ? (
-            <Loading />
-          ) : status === 'error' ? (
+          {status === 'error' ? (
             <ErrorList error={error} />
           ) : (
             <>
               <Select
-                options={liveOptions}
+                options={options}
                 isOptionDisabled={option =>
                   data.some(deck => {
-                    return deck.node.lang === option.value
+                    return status === 'loading'
+                      ? // while loading the list of decks, all options enabled
+                        false
+                      : // otherwise, disable languages we're already learning
+                        deck.node.lang === option.value
                   })
                 }
                 autoFocus
-                closeMenuOnSelect
-                escapeClearsValue
-                onChange={e => setValue(e.value)}
+                placeholder="Select a language..."
+                backspaceRemovesValue
+                area-label="Select a language to start a new deck"
+                onChange={handleChange}
               />
-              <button type="submit" className="my-6 btn rounded btn-primary">
+              {formError ? <TinyError text={formError} /> : null}
+              <button
+                type="submit"
+                className="my-6 btn rounded btn-primary"
+                disabled={isDisabled}
+                aria-disabled={isDisabled}
+              >
                 Start learning {languages[value]}
               </button>
             </>
