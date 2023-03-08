177d3dd9d411d0ea7d145870c2f354f5cc879254
Michael Snook <me@michaelsnook.com>
2023-03-08 15:59:03 +0530

    Make the ErrorList comp a bit more defensive

--- components/ErrorList.js
+++ components/ErrorList.js
@@ -1,13 +1,17 @@
 const ErrorList = ({ summary, error, errors, asCard }) => {
   // convert neively-passed error objects to strings
-  const errorString = typeof error === 'object' ? JSON.stringify(error) : error
+  const errorString = JSON.stringify(error)
 
   return !error && !errors?.length ? null : (
     <div className={`${asCard ? 'big-card' : ''} mt-6 mb-2`}>
       {summary ? <p className="font-bold text-red-600">{summary}</p> : null}
       <ul className="pl-5 text-red-600 list-disc">
         {errorString ? <li key={errorString}>{errorString}</li> : null}
-        {errors ? errors.map(m => <li key={m}>{m}</li>) : null}
+        {errors
+          ? errors.map(m => (
+              <li key={JSON.stringify(m)}>{JSON.stringify(m)}</li>
+            ))
+          : null}
       </ul>
     </div>
   )
