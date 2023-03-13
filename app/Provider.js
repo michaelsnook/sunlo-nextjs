e4c553405e8c71d5dccd93d3dfbfca408fc9b4ad
Michael Snook <me@michaelsnook.com>
2023-03-13 20:40:29 +0530

    Add config for typescript & schema.graphql copied from API

--- app/Provider.js
+++ app/Provider.js
@@ -1,15 +1,12 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
 export default function Provider({ children }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
 }
--- app/data/queries.js
+++ app/data/queries.js
@@ -209,6 +209,7 @@ export const phraseDetailsQuery = gql`
     }
   }
 `
+
 export const profileQuery = gql`
   query ProfileQuery {
     profileCollection {
