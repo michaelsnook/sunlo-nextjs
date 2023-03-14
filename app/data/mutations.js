38680d3ee94a38abb52fc2ba434cce719c441e00
Michael Snook <me@michaelsnook.com>
2023-03-14 20:03:30 +0530

    Switch to generated TypedDocument queries

---
+++ app/data/mutations.js
@@ -0,0 +1,27 @@
+import { graphql } from 'app/data/gql/gql'
+
+export const newDeckMutation = graphql(/* GraphQL */ `
+  mutation Mutation($objects: [UserDeckInsertInput!]!) {
+    insertIntoUserDeckCollection(objects: $objects) {
+      records {
+        id
+        lang
+      }
+    }
+  }
+`)
+
+export const newCardMutation = graphql(/* GraphQL */ `
+  mutation InsertIntoDeckMembershipCollection(
+    $objects: [DeckMembershipInsertInput!]!
+  ) {
+    insertIntoDeckMembershipCollection(objects: $objects) {
+      records {
+        id
+        status
+        cardPhraseId
+        deckId
+      }
+    }
+  }
+`)
