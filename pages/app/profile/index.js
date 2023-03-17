d789d5ee63e9d1c078574bde85ee0e6235db8728
Michael Snook <me@michaelsnook.com>
2023-03-18 00:18:23 +0530

    Move loading bar inside layout

--- pages/app/profile/index.js
+++ pages/app/profile/index.js
@@ -42,82 +42,87 @@ const ProfileCard = () => {
       })
   }
 
-  return !profile || isLoading ? (
-    <Loading />
-  ) : (
+  return (
     <form className="big-card flex flex-col space-y-4" onSubmit={onSubmit}>
       <h2 className="h3">Profile</h2>
-      <fieldset
-        className="grid grid-cols-1 sm:grid-cols-2 gap-4"
-        disabled={!profile || isSubmitting}
-      >
-        <div className="flex flex-col">
-          <label htmlFor="username" className="font-bold px-3">
-            Your nickname
-          </label>
-          <input
-            id="username"
-            type="text"
-            className="border rounded p-3"
-            name="username"
-            tabIndex="1"
-            defaultValue={profile?.username || ''}
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="language_primary" className="font-bold px-3">
-            Primary language
-          </label>
-          <select
-            name="language_primary"
-            defaultValue={profile.language_primary || ''}
-            className="border rounded p-3"
-          >
-            <option value="">-- select one --</option>
-            {Object.keys(languages).map(k => (
-              <option key={`language-primary-${k}`} value={k}>
-                {languages[k]}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="languages_spoken" className="font-bold px-3">
-            Languages you know
-          </label>
-          <div className="py-3 border rounded overflow-auto h-40">
-            {Object.keys(languages).map(k => (
-              <p key={`languages-spoken-${k}`} className="flex">
-                <label className="group-checked:bg-primary group-checked:text-white w-full px-3 py-1">
-                  <input
-                    type="checkbox"
-                    className={`rounded mr-2 focus:outline-gray-400 select-none checked-${
-                      profile.languages_spoken.indexOf(k) !== -1 ||
-                      k === profile.language_primary
-                    }`}
-                    value={k}
-                    name="languages_spoken"
-                    defaultChecked={
-                      profile.languages_spoken.indexOf(k) !== -1 ||
-                      k === profile.language_primary
-                    }
-                  />
-                  {languages[k]}
-                </label>
-              </p>
-            ))}
+      {!profile || isLoading ? (
+        <Loading />
+      ) : (
+        <fieldset
+          className="grid grid-cols-1 sm:grid-cols-2 gap-4"
+          disabled={!profile || isSubmitting}
+        >
+          <div className="flex flex-col">
+            <label htmlFor="username" className="font-bold px-3">
+              Your nickname
+            </label>
+            <input
+              id="username"
+              type="text"
+              className="border rounded p-3"
+              name="username"
+              tabIndex="1"
+              defaultValue={profile?.username || ''}
+            />
           </div>
-        </div>
-        <div className="flex flex-col-reverse">
-          <button
-            className="btn btn-primary"
-            disabled={!profile || isSubmitting}
-          >
-            Save changes
-          </button>
-        </div>
-        <ErrorList summary="Problem updating profile" error={errors?.message} />
-      </fieldset>
+          <div className="flex flex-col">
+            <label htmlFor="language_primary" className="font-bold px-3">
+              Primary language
+            </label>
+            <select
+              name="language_primary"
+              defaultValue={profile.language_primary || ''}
+              className="border rounded p-3"
+            >
+              <option value="">-- select one --</option>
+              {Object.keys(languages).map(k => (
+                <option key={`language-primary-${k}`} value={k}>
+                  {languages[k]}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="languages_spoken" className="font-bold px-3">
+              Languages you know
+            </label>
+            <div className="py-3 border rounded overflow-auto h-40">
+              {Object.keys(languages).map(k => (
+                <p key={`languages-spoken-${k}`} className="flex">
+                  <label className="group-checked:bg-primary group-checked:text-white w-full px-3 py-1">
+                    <input
+                      type="checkbox"
+                      className={`rounded mr-2 focus:outline-gray-400 select-none checked-${
+                        profile.languages_spoken.indexOf(k) !== -1 ||
+                        k === profile.language_primary
+                      }`}
+                      value={k}
+                      name="languages_spoken"
+                      defaultChecked={
+                        profile.languages_spoken.indexOf(k) !== -1 ||
+                        k === profile.language_primary
+                      }
+                    />
+                    {languages[k]}
+                  </label>
+                </p>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col-reverse">
+            <button
+              className="btn btn-primary"
+              disabled={!profile || isSubmitting}
+            >
+              Save changes
+            </button>
+          </div>
+          <ErrorList
+            summary="Problem updating profile"
+            error={errors?.message}
+          />
+        </fieldset>
+      )}
     </form>
   )
 }
