68c2cc0a39ca4e7bd0fcde882449580808cc984b
Michael Snook <me@michaelsnook.com>
2023-03-17 14:19:17 +0530

    Remove extraneous gap between sidebar and main
    Let the main page/component handle its own padding instead.

--- app/(site)/(browse)/layout.js
+++ app/(site)/(browse)/layout.js
@@ -2,7 +2,7 @@ import Sidebar from 'app/components/Sidebar'
 
 export default function Layout({ children }) {
   return (
-    <div className="md:flex flex-row gap-6 bg-primary text-white">
+    <div className="md:flex flex-row bg-primary text-white">
       <Sidebar shy={true} />
       <div className="flex-grow py-6 px-min min-h-60vh">{children}</div>
     </div>
