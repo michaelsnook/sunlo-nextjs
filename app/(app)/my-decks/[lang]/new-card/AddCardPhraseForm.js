e0d38ad26eb4704521bb9a00e3e73c4a2c48ae23
Michael Snook <me@michaelsnook.com>
2023-03-20 18:52:08 +0530

    Add form for new deck/phrase/translations
    This PR also incldues some minor fixups on the GraphQL schema

--- app/(app)/my-decks/[lang]/new-card/AddCardPhraseForm.js
+++ app/(app)/my-decks/[lang]/new-card/AddCardPhraseForm.js
@@ -1,16 +1,60 @@
 'use client'
 
+import { useState } from 'react'
 import Link from 'next/link'
-import languages, { options } from 'lib/languages'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import Select from 'react-select'
+import languages, { options } from 'lib/languages'
 import { useDeck } from 'app/data/hooks'
+import ErrorList from 'components/ErrorList'
+import Loading from 'app/loading'
+import { postNewPhraseCardTranslations } from 'app/data/posters'
 
 export default function AddCardPhraseForm({ lang }) {
-  const { status, data, error } = useDeck(lang)
+  const { status, data: deck, error } = useDeck(lang)
+  const [selectLang, setSelectLang] = useState()
+  const queryClient = useQueryClient()
 
-  console.log(data)
+  const addCardPhrase = useMutation({
+    mutationFn: postNewPhraseCardTranslations,
+    onSuccess: data => {
+      console.log(`postNewPhraseCardTranslations success`, data)
+      queryClient.invalidateQueries({ queryKey: ['user_deck', lang] })
+    },
+    onError: error => {
+      throw error
+    },
+  })
+
+  const handleChangeSelect = ({ value }) => {
+    setSelectLang(value)
+  }
+
   const handleSubmit = event => {
-    console.log(event)
+    event.preventDefault()
+    // console.log(event.target)
+
+    if (!deck?.id) {
+      throw 'wait error'
+    }
+    if (!selectLang?.length === 3) throw 'bad translation_lang'
+    if (!event.target.text.value) throw 'no phrase text'
+    if (!event.target.translation_text.value) throw 'no translation text'
+
+    const submitData = {
+      phrase: {
+        text: event.target.text.value,
+        lang,
+      },
+      phraseTranslations: [
+        {
+          text: event.target.translation_text.value,
+          lang: selectLang,
+        },
+      ],
+      userDeckId: deck.id,
+    }
+    addCardPhrase.mutate(submitData)
   }
   return (
     <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
@@ -36,20 +80,52 @@ export default function AddCardPhraseForm({ lang }) {
             options={options}
             isOptionDisabled={option => option.value === lang}
             placeholder="Select a language..."
+            onChange={handleChangeSelect}
             aria-label="Select a language for your translation"
           />
         </div>
       </div>
       <div className="flex justify-between">
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-        <Link
-          className="place-self-center btn btn-quiet"
-          href={`/my-decks/${lang}`}
-        >
-          Cancel
-        </Link>
+        {addCardPhrase.isLoading ? (
+          <Loading />
+        ) : addCardPhrase.isError ? (
+          <ErrorList error={addCardPhrase.error} />
+        ) : addCardPhrase.isSuccess ? (
+          <p className="text-lg">
+            Success! added this new phrase to your deck!{' '}
+            <Link
+              href={`/my-decks/${lang}`}
+              className="text-primary hover:underline"
+            >
+              Go back
+            </Link>{' '}
+            or add{' '}
+            <a
+              href="#"
+              className="text-primary hover:underline"
+              onClick={() => location.reload()}
+            >
+              another new card
+            </a>
+            .
+          </p>
+        ) : (
+          <>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={!status === 'success'}
+            >
+              Submit
+            </button>
+            <Link
+              className="place-self-center btn btn-quiet"
+              href={`/my-decks/${lang}`}
+            >
+              Cancel
+            </Link>
+          </>
+        )}
       </div>
     </form>
   )
--- app/(app)/my-decks/[lang]/page.js
+++ app/(app)/my-decks/[lang]/page.js
@@ -8,7 +8,15 @@ export default function Page({ params: { lang } }) {
       <Link className="hover:underline" href="/my-decks">
         &larr; Back to decks
       </Link>
-      <h1 className="h1">Learn {languages[lang]}</h1>
+      <div className="flex flex-row justify-between">
+        <h1 className="h1">Learn {languages[lang]}</h1>
+        <Link
+          href={`/my-decks/${lang}/new-card`}
+          className="flex-none hover:underline place-self-center"
+        >
+          + new card
+        </Link>
+      </div>
       <div className="page-card">
         <ClientPage lang={lang} />
       </div>
