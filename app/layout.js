54bd66567dac59b3b266cf8e41ee7f1581bc77d3
Michael Snook <me@michaelsnook.com>
2023-03-24 01:33:09 +0530

    Fix charSet to JSX-case

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
