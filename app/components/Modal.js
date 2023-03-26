8047832249708878367b889cad11052d49dc3a45
Michael Snook <me@michaelsnook.com>
2023-03-26 17:07:27 +0530

    Prettify the backdrop, little dark, little blur

--- app/components/Modal.js
+++ app/components/Modal.js
@@ -5,8 +5,10 @@ Modal.setAppElement('#modal-root')
 function MyModal({ onRequestClose, isOpen, children }) {
   return (
     <Modal
-      className="big-card my-6 mx-auto"
       isOpen={isOpen}
+      className="big-card my-6 mx-auto"
+      noScroll={true}
+      overlayClassName="bg-black/60 fixed top-0 bottom-0 left-0 right-0 backdrop-blur-sm absolute"
       onRequestClose={onRequestClose}
     >
       {children}
