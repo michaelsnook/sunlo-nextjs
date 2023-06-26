2af6ab6f0818c6b328c4a147c0cc3a5bf040f762
Michael Snook <me@michaelsnook.com>
2023-06-26 18:18:07 +0530

    Fix incorrect query key

--- app/(app)/my-decks/new/page.js
+++ app/(app)/my-decks/new/page.js
@@ -26,7 +26,7 @@ export default function Page() {
     mutationFn: postNewDeck,
     onSuccess: data => {
       // console.log(`onSuccess data,`, data)
-      queryClient.invalidateQueries({ queryKey: ['decks'] })
+      queryClient.invalidateQueries({ queryKey: ['user_decks'] })
       router.push(`/my-decks/${data.lang}`)
     },
   })
