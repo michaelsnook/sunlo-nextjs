cd75502471e85b9b966a6cb6d259d61357899398
Michael Snook <me@michaelsnook.com>
2023-06-26 13:17:34 +0530

    Regenerate types

--- types/supabase.ts
+++ types/supabase.ts
@@ -25,6 +25,7 @@ export interface Database {
           lang?: string
           name?: string
         }
+        Relationships: []
       }
       phrase: {
         Row: {
@@ -45,6 +46,20 @@ export interface Database {
           lang?: string
           text?: string
         }
+        Relationships: [
+          {
+            foreignKeyName: "phrase_added_by_fkey"
+            columns: ["added_by"]
+            referencedRelation: "user_profile"
+            referencedColumns: ["uid"]
+          },
+          {
+            foreignKeyName: "phrase_lang_fkey"
+            columns: ["lang"]
+            referencedRelation: "language"
+            referencedColumns: ["lang"]
+          }
+        ]
       }
       phrase_see_also: {
         Row: {
@@ -65,6 +80,26 @@ export interface Database {
           id?: string
           to_phrase_id?: string | null
         }
+        Relationships: [
+          {
+            foreignKeyName: "phrase_see_also_added_by_fkey"
+            columns: ["added_by"]
+            referencedRelation: "user_profile"
+            referencedColumns: ["uid"]
+          },
+          {
+            foreignKeyName: "phrase_see_also_from_phrase_id_fkey"
+            columns: ["from_phrase_id"]
+            referencedRelation: "phrase"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "phrase_see_also_to_phrase_id_fkey"
+            columns: ["to_phrase_id"]
+            referencedRelation: "phrase"
+            referencedColumns: ["id"]
+          }
+        ]
       }
       phrase_translation: {
         Row: {
@@ -91,6 +126,26 @@ export interface Database {
           phrase_id?: string
           text?: string
         }
+        Relationships: [
+          {
+            foreignKeyName: "phrase_translation_added_by_fkey"
+            columns: ["added_by"]
+            referencedRelation: "user_profile"
+            referencedColumns: ["uid"]
+          },
+          {
+            foreignKeyName: "phrase_translation_lang_fkey"
+            columns: ["lang"]
+            referencedRelation: "language"
+            referencedColumns: ["lang"]
+          },
+          {
+            foreignKeyName: "phrase_translation_phrase_id_fkey"
+            columns: ["phrase_id"]
+            referencedRelation: "phrase"
+            referencedColumns: ["id"]
+          }
+        ]
       }
       user_card: {
         Row: {
@@ -117,6 +172,26 @@ export interface Database {
           updated_at?: string | null
           user_deck_id?: string
         }
+        Relationships: [
+          {
+            foreignKeyName: "user_card_phrase_id_fkey"
+            columns: ["phrase_id"]
+            referencedRelation: "phrase"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "user_card_uid_fkey"
+            columns: ["uid"]
+            referencedRelation: "user_profile"
+            referencedColumns: ["uid"]
+          },
+          {
+            foreignKeyName: "user_card_user_deck_id_fkey"
+            columns: ["user_deck_id"]
+            referencedRelation: "user_deck"
+            referencedColumns: ["id"]
+          }
+        ]
       }
       user_deck: {
         Row: {
@@ -134,6 +209,20 @@ export interface Database {
           lang?: string
           uid?: string
         }
+        Relationships: [
+          {
+            foreignKeyName: "user_deck_lang_fkey"
+            columns: ["lang"]
+            referencedRelation: "language"
+            referencedColumns: ["lang"]
+          },
+          {
+            foreignKeyName: "user_deck_uid_fkey"
+            columns: ["uid"]
+            referencedRelation: "user_profile"
+            referencedColumns: ["uid"]
+          }
+        ]
       }
       user_profile: {
         Row: {
@@ -163,6 +252,7 @@ export interface Database {
           updated_at?: string | null
           username?: string | null
         }
+        Relationships: []
       }
     }
     Views: {
@@ -170,6 +260,170 @@ export interface Database {
     }
     Functions: {
       [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+  storage: {
+    Tables: {
+      buckets: {
+        Row: {
+          created_at: string | null
+          id: string
+          name: string
+          owner: string | null
+          public: boolean | null
+          updated_at: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id: string
+          name: string
+          owner?: string | null
+          public?: boolean | null
+          updated_at?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: string
+          name?: string
+          owner?: string | null
+          public?: boolean | null
+          updated_at?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "buckets_owner_fkey"
+            columns: ["owner"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      migrations: {
+        Row: {
+          executed_at: string | null
+          hash: string
+          id: number
+          name: string
+        }
+        Insert: {
+          executed_at?: string | null
+          hash: string
+          id: number
+          name: string
+        }
+        Update: {
+          executed_at?: string | null
+          hash?: string
+          id?: number
+          name?: string
+        }
+        Relationships: []
+      }
+      objects: {
+        Row: {
+          bucket_id: string | null
+          created_at: string | null
+          id: string
+          last_accessed_at: string | null
+          metadata: Json | null
+          name: string | null
+          owner: string | null
+          path_tokens: string[] | null
+          updated_at: string | null
+        }
+        Insert: {
+          bucket_id?: string | null
+          created_at?: string | null
+          id?: string
+          last_accessed_at?: string | null
+          metadata?: Json | null
+          name?: string | null
+          owner?: string | null
+          path_tokens?: string[] | null
+          updated_at?: string | null
+        }
+        Update: {
+          bucket_id?: string | null
+          created_at?: string | null
+          id?: string
+          last_accessed_at?: string | null
+          metadata?: Json | null
+          name?: string | null
+          owner?: string | null
+          path_tokens?: string[] | null
+          updated_at?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "objects_bucketId_fkey"
+            columns: ["bucket_id"]
+            referencedRelation: "buckets"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "objects_owner_fkey"
+            columns: ["owner"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      extension: {
+        Args: {
+          name: string
+        }
+        Returns: string
+      }
+      filename: {
+        Args: {
+          name: string
+        }
+        Returns: string
+      }
+      foldername: {
+        Args: {
+          name: string
+        }
+        Returns: unknown
+      }
+      get_size_by_bucket: {
+        Args: Record<PropertyKey, never>
+        Returns: {
+          size: number
+          bucket_id: string
+        }[]
+      }
+      search: {
+        Args: {
+          prefix: string
+          bucketname: string
+          limits?: number
+          levels?: number
+          offsets?: number
+          search?: string
+          sortcolumn?: string
+          sortorder?: string
+        }
+        Returns: {
+          name: string
+          id: string
+          updated_at: string
+          created_at: string
+          last_accessed_at: string
+          metadata: Json
+        }[]
+      }
     }
     Enums: {
       [_ in never]: never
