01b0054d4267fc8b1ec3956560e7908f0966069d
Michael Snook <me@michaelsnook.com>
2023-06-26 19:14:50 +0530

    Remove profile, decks, and related helpers from global context
    The Global Context Providers is now _only_ providing session, user,
    and the signIn signOut helpers. It can probably be renamed to
    userContext or something like that, but I may replace it altogether
    with next auth helpers or a hook.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import 'styles/globals.css'
 import { GlobalStateProvider } from 'lib/global-store'
+import Provider from 'app/Provider'
 
 function App({ Component, pageProps }) {
   return (
-    <GlobalStateProvider>
-      <Component {...pageProps} />
-    </GlobalStateProvider>
+    <Provider>
+      <GlobalStateProvider>
+        <Component {...pageProps} />
+      </GlobalStateProvider>
+    </Provider>
   )
 }
 
