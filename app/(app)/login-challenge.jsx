40c89092ee4ed8cf7ae29ba5696eb6b44ddc37a8
Michael Snook <michael.snook@gmail.com>
2024-05-12 00:57:07 +0530

    Separate these two modal components
    It's maybe too early to assume these will act the same.

--- app/(app)/login-challenge.jsx
+++ app/(app)/login-challenge.jsx
@@ -1,15 +1,25 @@
 'use client'
 
+import { useState } from 'react'
+import Modal from 'react-modal'
 import { useAuth } from 'lib/auth-context'
 import Login from 'components/LoginForm'
-import MyModal from 'app/components/Modal'
+
+Modal.setAppElement('#modal-root')
 
 export default function LoginChallenge() {
   const { isAuth } = useAuth()
+  const [isDismissed, setIsDismissed] = useState()
   // console.log(`auth login challenge`, isAuth)
-  return isAuth ? null : (
-    <MyModal isOpen={true} onRequestClose={() => {}}>
+  return (
+    <Modal
+      isOpen={!isDismissed && !isAuth}
+      className="big-card my-6 mx-auto w-11/12 place-self-center outline-none"
+      overlayClassName="bg-black/60 fixed backdrop-blur-sm fixed inset-0 flex"
+      noScroll={true}
+      handleCloseModal={() => setIsDismissed(true)}
+    >
       <Login asModal />
-    </MyModal>
+    </Modal>
   )
 }
