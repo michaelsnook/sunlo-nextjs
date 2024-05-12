b0975fc3b6850f5b99b0d9fc8b5fb43748a98541
Michael Snook <michael.snook@gmail.com>
2024-05-12 16:58:51 +0530

    Set metadata correctly in RootLayout

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,13 +3,19 @@ import { AuthProvider } from 'lib/auth-context'
 import { Toaster } from 'react-hot-toast'
 import 'styles/globals.css'
 
+export const metadata = {
+  image: `/images/sunlo-logo-color.png`,
+  description: `Sunlo is a Social Language Learning App. Build a deck of flash cards, or help a friend learn phrases that will be useful from day one.`,
+  title: `Sunlo, the Social Language Learning App`,
+}
+
+export const viewport = {
+  themeColor: '#570df8',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
       <body>
         <Toaster />
         <QueryProvider>
