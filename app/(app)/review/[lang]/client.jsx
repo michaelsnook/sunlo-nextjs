3a39979f4aef4f7dab5f25b9cbf43d5d3224b4f1
Michael Snook <michael.snook@gmail.com>
2024-05-18 04:49:04 +0530

    Split the CardInner and mutation into its own file

--- app/(app)/review/[lang]/client.jsx
+++ app/(app)/review/[lang]/client.jsx
@@ -1,14 +1,11 @@
 'use client'
 
 import { useMemo, useState } from 'react'
-import { useMutation } from '@tanstack/react-query'
 import languages from 'lib/languages'
 import { useDeck } from 'app/data/hooks'
 import Loading from 'app/loading'
-import supabase from 'lib/supabase-client'
-import ErrorList from 'app/components/ErrorList'
-import { toast } from 'react-hot-toast'
 import SuccessCheckmark from 'app/components/SvgComponents'
+import CardInner from './card'
 
 function shuffle(array) {
   if (!array?.length > 0) return []
@@ -27,116 +24,6 @@ const Empty = () => (
     pls.
   </p>
 )
-
-const postReview = async ({ card_id, score, prevId }) => {
-  if (!card_id || !score) throw Error('Invalid review; cannot log')
-
-  const { data, error } = prevId
-    ? await supabase
-        .from('user_card_review')
-        .update({ score })
-        .eq('id', prevId)
-        .select()
-    : await supabase
-        .from('user_card_review')
-        .insert({ card_id, score })
-        .select()
-
-  if (error) throw Error(error)
-  // console.log(`We posted the review,`, data, error)
-  return data[0]
-}
-
-const CardInner = ({ card, advance, addReview, hidden }) => {
-  const [isRevealed, setIsRevealed] = useState(false)
-  const reveal = () => {
-    setIsRevealed(true)
-  }
-
-  const { data, error, mutate, status } = useMutation({
-    mutationFn: submission =>
-      postReview({ ...submission, card_id: card.id, prevId: data?.id }),
-    onSuccess: result => {
-      // console.log(`onSuccess firing with`, result)
-      addReview(result)
-      if (result.score === -2)
-        toast('got it', { icon: '👍️', position: 'bottom-center' })
-      if (result.score === -1)
-        toast('got it', { icon: '👍️', position: 'bottom-center' })
-      if (result.score === 1)
-        toast('got it', { icon: '👍️', position: 'bottom-center' })
-      if (result.score === 2)
-        toast.success('got it', { position: 'bottom-center' })
-      setTimeout(advance, 2000)
-    },
-  })
-
-  return hidden ? null : (
-    <div className="big-card">
-      <div className="flex flex-col justify-center text-center gap-8">
-        <h2 className="h2 text-center">{card?.phrase?.text}</h2>
-        {status === 'loading' ? (
-          <div className="absolute bg-white/70 top-0 left-0 right-0 bottom-0 content-center">
-            <Loading />
-          </div>
-        ) : null}
-        {status === 'error' ? (
-          <div className="absolute bg-white/50 top-0 left-0 right-0 bottom-0">
-            <ErrorList error={error} />
-          </div>
-        ) : null}
-        {!isRevealed ? (
-          <div className="flex gap-4 justify-center">
-            <button className="btn btn-success" onClick={reveal}>
-              Yes I know it
-            </button>
-            <button className="btn btn-warning" onClick={reveal}>
-              I don&apos;t know it
-            </button>
-          </div>
-        ) : (
-          <>
-            <div>
-              {card.phrase.translations.map(t => (
-                <p key={t.id}>&ldquo;{t.text}&rdquo;</p>
-              ))}
-            </div>
-            <div className="flex gap-4 justify-center">
-              <button
-                className={`btn btn-success ${data ? 'btn-outline' : ''}`}
-                onClick={() => mutate({ score: 2 })}
-                disabled={data?.score === 2}
-              >
-                Nailed it!
-              </button>
-              <button
-                className={`btn btn-info ${data ? 'btn-outline' : ''}`}
-                onClick={() => mutate({ score: 1 })}
-                disabled={data?.score === 1}
-              >
-                Got it
-              </button>
-              <button
-                className={`btn btn-warning ${data ? 'btn-outline' : ''}`}
-                onClick={() => mutate({ score: -1 })}
-                disabled={data?.score === -1}
-              >
-                It was hard
-              </button>
-              <button
-                className={`btn btn-error ${data ? 'btn-outline' : ''}`}
-                onClick={() => mutate({ score: -2 })}
-                disabled={data?.score === -2}
-              >
-                Didn&apos;t get it
-              </button>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
-  )
-}
 
 export default function ClientPage({ lang }) {
   const { data, status } = useDeck(lang)
