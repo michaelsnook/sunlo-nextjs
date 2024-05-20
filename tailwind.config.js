ecc2770abd409ce9c6f00f565b2b740d0418df2f
Michael Snook <michael.snook@gmail.com>
2024-05-21 00:34:30 +0530

    Update dependencies to zoom past some weird bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,6 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/forms'),
     require('daisyui'),
   ],
