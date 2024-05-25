27d9445277e1e716d61b751bf94ac0a65b2b94c4
Michael Snook <michael.snook@gmail.com>
2024-05-25 18:29:16 +0530

    Clean up add-new-deck: colocate function, use mutation
    This cleanup moves the submit function out of `posters.ts` and colocates
    it alongside the components/routes that use it, as `add-new-deck.ts`.
    
    I enjoy this pattern, as the functions are only being used once, but they
    could be re-used elsewhere, and regardless it's still nice to give them their
    own space, and to be able to use typescript.
    
    It also separates the page into server side page.jsx and the 'use client'
    component in form.jsx.

---
+++ app/(app)/my-decks/new/add-new-deck.ts
@@ -0,0 +1,17 @@
+'use client'
+
+import supabase from 'lib/supabase-client'
+import { DeckStub } from 'types/client-types'
+
+export const postNewDeck = async (lang: string): Promise<DeckStub | null> => {
+  // console.log(`postNewDeck ${lang}`)
+  if (typeof lang !== 'string' || lang.length !== 3)
+    throw 'Form not right. Maybe refresh. Or tell the devs.'
+  const { data, error } = await supabase
+    .from('user_deck')
+    .insert({ lang })
+    .select()
+  // console.log(`postNewDeck`, data, error)
+  if (error) throw error
+  return data[0] ?? null
+}
---
+++ app/(app)/my-decks/new/form.jsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { useState } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import Select from 'react-select'
+import { toast } from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
+import { useAllDecks } from 'app/data/hooks'
+import ErrorList from 'app/components/ErrorList'
+import languages, { allLanguageOptions } from 'lib/languages'
+import { postNewDeck } from './add-new-deck'
+
+function TinyError({ text }) {
+  return <p className="my-4 text-error/70">{text}</p>
+}
+
+export default function Form() {
+  const [lang, setLang] = useState()
+  const router = useRouter()
+  const queryClient = useQueryClient()
+
+  const createNewDeck = useMutation({
+    mutationFn: async event => {
+      event.preventDefault()
+      return await postNewDeck(lang)
+    },
+    onSuccess: data => {
+      // console.log(`onSuccess data,`, data)
+      queryClient.invalidateQueries({ queryKey: ['user_decks'] })
+      toast.success(`Created a new deck to learn ${languages[data.lang]}`)
+      router.push(`/my-decks/${data.lang}`)
+    },
+  })
+
+  const { data, error, status } = useAllDecks()
+
+  return (
+    <div>
+      {createNewDeck?.error ? (
+        <ErrorList summary={`${createNewDeck.error}`} />
+      ) : (
+        status === 'error' && <ErrorList summary={`${error.message}`} />
+      )}
+      <form name="new-deck" onSubmit={createNewDeck.mutate}>
+        <Select
+          options={allLanguageOptions}
+          isOptionDisabled={option =>
+            data?.some(deck => {
+              return status === 'loading'
+                ? // while loading the list of decks, all options enabled
+                  false
+                : // otherwise, disable languages we're already learning
+                  deck.lang === option.value
+            })
+          }
+          placeholder="Select a language..."
+          backspaceRemovesValue
+          aria-label="Select a language to start a new deck"
+          onChange={event => setLang(event.value)}
+        />
+        <button type="submit" className="my-6 btn rounded btn-primary">
+          Start learning
+        </button>
+      </form>
+      {createNewDeck.error && <TinyError text={`${createNewDeck.error}`} />}
+    </div>
+  )
+}
