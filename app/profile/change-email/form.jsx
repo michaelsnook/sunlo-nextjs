527ce92611ec86132898a0d1e60f009d1a687956
Michael Snook <michael.snook@gmail.com>
2024-05-27 05:33:54 +0530

    Use useMutation for change email form

--- app/profile/change-email/form.jsx
+++ app/profile/change-email/form.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useMutation } from '@tanstack/react-query'
 import supabase from 'lib/supabase-client'
 import { useAuth } from 'lib/auth-context'
 import ErrorList from 'app/components/ErrorList'
@@ -8,43 +8,33 @@ import { BASE_URL } from 'lib/helpers'
 
 export default function SetNewEmailForm() {
   const { user } = useAuth()
-  const [errors, setErrors] = useState()
-  const [isSubmitting, setIsSubmitting] = useState()
-  const [successfulSubmit, setSuccessfulSubmit] = useState()
 
-  const onSubmit = event => {
-    setErrors()
-    setIsSubmitting(true)
-    event.preventDefault()
-
-    const email = event.target.email.value
-
-    supabase.auth
-      .updateUser({
+  const changeEmail = useMutation({
+    mutationFn: async event => {
+      event.preventDefault()
+      const email = event.target.email.value
+      const { data, error } = await supabase.auth.updateUser({
         email,
         options: {
           emailRedirectTo: `${BASE_URL}/change-email-success`,
         },
       })
-      .then(({ data, error }) => {
-        console.log(data, error)
-        setIsSubmitting(false)
-        setSuccessfulSubmit(!error)
-        setErrors(error)
-      })
-  }
+      if (error) throw error
+      return data
+    },
+  })
 
   return (
     <div className="section-card-inner">
-      {successfulSubmit ? (
+      {changeEmail.isSuccess ? (
         <SuccessfulSubmit />
       ) : (
         <>
           <h1 className="h3 text-base-content/90">Choose a new email</h1>
-          <form role="form" onSubmit={onSubmit} className="form">
+          <form role="form" onSubmit={changeEmail.mutate} className="form">
             <fieldset
               className="flex flex-col gap-y-4"
-              disabled={!user || isSubmitting}
+              disabled={!user || changeEmail.isSubmitting}
             >
               <div>
                 <p>
@@ -54,9 +44,11 @@ export default function SetNewEmailForm() {
                   id="email"
                   name="email"
                   required="required"
-                  aria-invalid={errors?.email ? 'true' : 'false'}
+                  aria-invalid={
+                    changeEmail.error?.errors?.email ? 'true' : 'false'
+                  }
                   className={`${
-                    errors?.email ? 'border-error/60' : ''
+                    changeEmail.error?.errors?.email ? 'border-error/60' : ''
                   } rounded-md w-full`}
                   tabIndex="1"
                   type="text"
@@ -69,8 +61,8 @@ export default function SetNewEmailForm() {
                   tabIndex="3"
                   className="btn btn-primary"
                   type="submit"
-                  disabled={!user || isSubmitting}
-                  aria-disabled={isSubmitting}
+                  disabled={!user || changeEmail.isSubmitting}
+                  aria-disabled={changeEmail.isSubmitting}
                 >
                   Set new Email
                 </button>
@@ -79,7 +71,7 @@ export default function SetNewEmailForm() {
           </form>
           <ErrorList
             summary="Error requesting new email"
-            error={errors?.message}
+            error={changeEmail.error}
           />
         </>
       )}
