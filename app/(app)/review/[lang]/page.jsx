9916fe97cd6858613af7340c754b2bdd7a40749f
Michael Snook <michael.snook@gmail.com>
2024-06-02 21:03:22 +0530

    Add generateStaticParams to language/[lang]

--- app/(app)/review/[lang]/page.jsx
+++ app/(app)/review/[lang]/page.jsx
@@ -1,5 +1,12 @@
+import languages from 'lib/languages'
 import ClientPage from './client'
 
 export default function Page({ params: { lang } }) {
   return <ClientPage lang={lang} />
+}
+
+export async function generateStaticParams() {
+  return Object.keys(languages).map(lang => ({
+    lang,
+  }))
 }
