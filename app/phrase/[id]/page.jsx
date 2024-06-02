4e215d6f6645649a67f794afcc5278a8b156eccc
Michael Snook <michael.snook@gmail.com>
2024-06-02 21:20:03 +0530

    Fix issues building phrase/[id] when phrase not exactly found

--- app/phrase/[id]/page.jsx
+++ app/phrase/[id]/page.jsx
@@ -2,17 +2,23 @@ import Link from 'next/link'
 import { getAllPhraseDetails } from 'app/data/fetchers'
 import TinyPhrase from 'app/components/TinyPhrase'
 import languages from 'lib/languages'
+import { notFound } from 'next/navigation'
 
 export async function generateStaticParams() {
   let phrases = await getAllPhraseDetails()
-  return phrases.map(phrase => ({
-    id: phrase.id,
-  }))
+  return phrases.map(phrase => {
+    return phrase?.lang && phrase?.text && phrase?.id
+      ? {
+          id: phrase.id,
+        }
+      : null
+  })
 }
 
-export default async function Page({ params }) {
+export default async function Page({ params: { id } }) {
   const phrases = await getAllPhraseDetails()
-  const phrase = phrases.find(p => p.id === params.id)
+  const phrase = phrases.find(p => p.id === id)
+  if (!phrase) notFound()
 
   return (
     <div className="page-card gap-8 lg:w-[50vw] w-96">
