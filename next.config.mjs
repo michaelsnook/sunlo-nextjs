03e4db76d095d0947e81f75615f53cd089d2275b
Michael Snook <michael.snook@gmail.com>
2024-06-07 17:21:27 +0530

    Add some logic for Tauri to NextJS config
    This also changes the config js to mjs: `next.config.mjs`

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
+let internalHost = null
+
+if (!isProd) {
+  const { internalIpV4 } = await import('internal-ip')
+  internalHost = await internalIpV4()
+}
+
+const assetPrefixSpreadable = isProd
+  ? {}
+  : { assetPrefix: `http://${internalHost}:3000` }
+
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  reactStrictMode: true,
+  ...assetPrefixSpreadable,
+}
+
+export default nextConfig
