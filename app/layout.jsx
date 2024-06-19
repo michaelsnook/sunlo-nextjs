2c0e8a460d1f20b472fa19a77489302674c1b1e6
Em Snook <me@michaelsnook.com>
2024-06-20 01:12:45 +0530

    Simplify the layout some more

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,37 +17,33 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary text-white">
+      <body className="bg-primary text-white flex flex-row min-h-screen">
         <Toaster />
         <QueryProvider>
           <AuthProvider>
-            <div id="modal-root" />
             {/*
               This is the root of the layout. The flex, with 2 children
               a) Sidebar with fixed width, and b) and main content/app area
               with .flex-grow
             */}
-            <div className="flex min-h-screen">
-              <Sidebar />
-              {/*
-                This is the main container for the app. Its child applies some
-                container-relative padding and width, ensuring that the app's
-                business happens in a consistent sizing box, no matter the
-                contents on screen at the time.
-              */}
-              <div className="@container w-full py-8">
-                <div
-                  className="
-                    w-[98cqw] max-w-[900px] px-[1cqw]
-                    mx-auto
-                    min-h-screen
-                    flex flex-col
-                  "
-                >
-                  {children}
-                </div>
-              </div>
+            <Sidebar />
+            {/*
+              This is the main container for the app. Its child applies some
+              container-relative padding and width, ensuring that the app's
+              business happens in a consistent sizing box, no matter the
+              contents on screen at the time.
+            */}
+            <div
+              className="
+                @container w-full max-w-[1100px] 
+                py-20 
+                mx-auto px-[1%]
+                flex flex-col
+              "
+            >
+              {children}
             </div>
+            <div id="modal-root" />
           </AuthProvider>
         </QueryProvider>
       </body>
