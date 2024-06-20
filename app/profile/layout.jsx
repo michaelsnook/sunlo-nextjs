b348a8ba5a1d2a0285e6e076a9c959399ca046bc
Em Snook <me@michaelsnook.com>
2024-06-20 17:57:15 +0530

    Add navbar to profile screen

--- app/profile/layout.jsx
+++ app/profile/layout.jsx
@@ -2,7 +2,7 @@ import AvatarSection from './avatar-section'
 
 export default function Layout({ children }) {
   return (
-    <div className="w-app space-y-4">
+    <div className="w-app flex flex-col gap-4">
       <AvatarSection />
       {children}
     </div>
