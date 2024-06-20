8913d672ae5946d80d1b50e344fd5eae16239df8
Em Snook <me@michaelsnook.com>
2024-06-20 13:50:22 +0530

    Add some more explicit styles to components below the layout

--- app/profile/change-password/page.jsx
+++ app/profile/change-password/page.jsx
@@ -2,7 +2,7 @@ import SetNewPasswordForm from 'app/(auth)/set-new-password/form'
 
 export default function Page() {
   return (
-    <main className="card card-body bg-base-100 text-base-content">
+    <main className="card card-body bg-base-100 text-base-content max-w-lg mx-auto">
       <SetNewPasswordForm />
     </main>
   )
