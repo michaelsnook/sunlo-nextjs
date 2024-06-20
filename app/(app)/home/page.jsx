2801b4bb421b0662a960fb2c647af92e1be63b3c
Em Snook <me@michaelsnook.com>
2024-06-20 17:53:10 +0530

    Add navbar to launch screen

--- app/(app)/home/page.jsx
+++ app/(app)/home/page.jsx
@@ -1,16 +1,19 @@
 import Link from 'next/link'
+import Navbar from '../Navbar'
 import Client from './client'
 
 export default function Page() {
   return (
-    <main className="flex flex-col gap-4 p-2">
-      <label className="label h2 text-center">Continue learning...</label>
-      <Client />
-      <div className="mx-auto">
-        <Link href={`/my-decks/new`}>
-          <span className="btn btn-ghost">+ Start a new language</span>
-        </Link>
-      </div>
-    </main>
+    <>
+      <Navbar title={`Continue learning...`} />
+      <main className="flex flex-col gap-4 p-2">
+        <Client />
+        <div className="mx-auto">
+          <Link href={`/my-decks/new`}>
+            <span className="btn btn-ghost">+ Start a new language</span>
+          </Link>
+        </div>
+      </main>
+    </>
   )
 }
