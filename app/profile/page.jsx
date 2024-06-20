6e0bfd696986b15d8bdbbc0f4c18c96ad44a30be
Em Snook <me@michaelsnook.com>
2024-06-20 17:57:15 +0530

    Add navbar to profile screen

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,8 +1,10 @@
+import Navbar from 'app/(app)/Navbar'
 import { ProfileCard, UserAuthCard } from './client'
 
 export default function Page() {
   return (
     <>
+      <Navbar title={`Your profile`} />
       <ProfileCard />
       <UserAuthCard />
     </>
