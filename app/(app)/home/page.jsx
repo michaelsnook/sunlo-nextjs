6c7c51b774ce4ae4c0b3e38b145684fcbd4a52fa
Em Snook <me@michaelsnook.com>
2024-06-20 22:04:29 +0530

    Handle the top and bottom safe zone padding for iOS notch

--- app/(app)/home/page.jsx
+++ app/(app)/home/page.jsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <>
       <Navbar title={`Continue learning...`}>
-        <Link href={`/my-decks/new`}>+ new neck</Link>
+        <Link href={`/my-decks/new`}>+ deck</Link>
       </Navbar>
       <main className="flex flex-col gap-4 p-2">
         <Client />
