4a962b6b1a2baea21e3a7ef2985af01d24b214fe
Em Snook <me@michaelsnook.com>
2024-06-27 00:02:22 +0530

    Don't use redirect on the client

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,12 +1,9 @@
 import LoginForm from './form'
-import ClientPage from './client'
 
 export default function Page() {
   return (
     <main className="card-white">
-      <ClientPage>
-        <LoginForm />
-      </ClientPage>
+      <LoginForm />
     </main>
   )
 }
