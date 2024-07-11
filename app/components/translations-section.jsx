d7f3939e592452f35981963d3349aa67ff93147d
Michael Snook <michael.snook@gmail.com>
2024-07-11 22:49:47 +0530

    Update alerts for Daisy v3 (and 4)

--- app/components/translations-section.jsx
+++ app/components/translations-section.jsx
@@ -82,24 +82,22 @@ export default function SectionTranslations({
             </button>
             {addTranslation.error && (
               <div className="alert alert-error">
-                <div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="stroke-current flex-shrink-0 h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                  {addTranslation.error.code === '23505'
-                    ? `This translation already exists for this phrase`
-                    : addTranslation.error.message}
-                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current flex-shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                {addTranslation.error.code === '23505'
+                  ? `This translation already exists for this phrase`
+                  : addTranslation.error.message}
               </div>
             )}
           </fieldset>
