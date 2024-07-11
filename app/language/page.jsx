45d77ec16bf1ef9d05bae2e71718f0102d1670e0
Michael Snook <michael.snook@gmail.com>
2024-07-11 21:56:11 +0530

    Separate 'empty' languages below

--- app/language/page.jsx
+++ app/language/page.jsx
@@ -6,13 +6,20 @@ export default async function Page() {
     .from('language_plus')
     .select('*')
     .is('alias_of', null)
+
+  const active = data.filter(l => l.phrases_to_learn > 0)
+  const inactive = data.filter(l => l.phrases_to_learn === 0)
+
   return (
     <main className="page-card">
       <h1 className="h1">Languages</h1>
-      <ul className="columns-1 @lg:columns-2 @3xl:columns-3 gap-4 space-y-4">
-        {data.map(({ lang, name, learners, phrases_to_learn }) => (
-          <li key={lang} className="card shadow text-start break-inside-avoid">
-            <Link href={`/language/${lang}`} className="card-body">
+      <ul className="columns-[18rem] gap-4 space-y-4">
+        {active.map(({ lang, name, learners, phrases_to_learn }) => (
+          <li key={lang} className="text-start break-inside-avoid">
+            <Link
+              href={`/language/${lang}`}
+              className="card-body hover:bg-base-200 card shadow"
+            >
               <h3 className="card-title">
                 {name} ({lang})
               </h3>
@@ -20,6 +27,20 @@ export default async function Page() {
                 {learners} {learners === 1 ? 'person' : 'people'} learning{' '}
                 {phrases_to_learn} different phrases
               </p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="divider" />
+      <p>
+        These languages are currently empty &ndash; you can be the first to
+        start adding cards to our public library
+      </p>
+      <ul className="columns-[12rem] gap-4 space-y-2 list-disc px-4">
+        {inactive.map(({ lang, name }) => (
+          <li key={lang}>
+            <Link href={`/language/${lang}`} className="s-link">
+              {name} ({lang})
             </Link>
           </li>
         ))}
