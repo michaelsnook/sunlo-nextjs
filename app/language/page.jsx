781c5789b026eecf0d22dc460f246b059bf4f902
Michael Snook <michael.snook@gmail.com>
2024-07-11 19:35:01 +0530

    Add language metadata to browse langauges page

--- app/language/page.jsx
+++ app/language/page.jsx
@@ -1,18 +1,22 @@
 import Link from 'next/link'
-import languages from 'lib/languages'
+import supabase from 'lib/supabase-client'
 
 export default async function Page() {
+  const { data, error } = await supabase.from('language_plus').select('*')
   return (
     <main className="page-card">
       <h1 className="h1">Languages</h1>
-      <ul className="columns-1 @lg:columns-2 @3xl:columns-3 gap-4">
-        {Object.keys(languages).map(lang => (
-          <li key={lang}>
-            <Link href={`/language/${lang}`} className="btn btn-ghost">
+      <ul className="columns-1 @lg:columns-2 @3xl:columns-3 gap-4 space-y-4">
+        {data.map(({ lang, name, learners, phrases_to_learn }) => (
+          <li key={lang} className="card shadow text-start break-inside-avoid">
+            <Link href={`/language/${lang}`} className="card-body">
+              <h3 className="card-title">
+                {name} ({lang})
+              </h3>
               <p>
-                {languages[lang]} ({lang})
+                {learners} {learners === 1 ? 'person' : 'people'} learning{' '}
+                {phrases_to_learn} different phrases
               </p>
-              {/*'<p>xx cards, yy users learning</p>'*/}
             </Link>
           </li>
         ))}
