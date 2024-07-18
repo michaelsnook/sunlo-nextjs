16071534dcb3a60500feb71250143240487b3007
Michael Snook <michael.snook@gmail.com>
2024-07-18 15:12:31 +0530

    Improve styling of these change email/pw link buttons

--- src/app/profile/user-auth-card.jsx
+++ src/app/profile/user-auth-card.jsx
@@ -10,28 +10,34 @@ export default function UserAuthCard() {
       <h2 className="card-title">Login credentials</h2>
       <div className="flex flex-col w-full">
         <label className="font-bold px-3">Your email</label>
-        <div className="flex flex-row">
+        <div className="flex flex-row gap-4">
           <input
             type="text"
             className="border rounded p-3 flex-grow bg-base-300 text-base-content/70"
             value={userEmail ?? 'loading...'}
             disabled
           />
-          <Link href="/profile/change-email" className="btn btn-link">
+          <Link
+            href="/profile/change-email"
+            className="btn btn-link hover:bg-base-200"
+          >
             Change
           </Link>
         </div>
       </div>
       <div className="flex flex-col">
         <label className="font-bold px-3">Your password</label>
-        <div className="flex flex-row">
+        <div className="flex flex-row gap-4">
           <input
             type="text"
             className="border rounded p-3 flex-grow bg-base-300 text-base-content/70"
             value="***************"
             disabled
           />
-          <Link href="/profile/change-password" className="btn btn-link">
+          <Link
+            href="/profile/change-password"
+            className="btn btn-link hover:bg-base-200"
+          >
             Change
           </Link>
         </div>
