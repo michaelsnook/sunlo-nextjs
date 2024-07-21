32ea4902dc6945a92912056d598aec2053fc42ae
Michael Snook <michael.snook@gmail.com>
2024-07-21 13:14:31 +0530

    Move everything into src and create tsconfig aliases

---
+++ src/app/(app)/my-decks/[lang]/phrase/[id]/client.jsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { AddCardButtonsSection, SectionSeeAlsos } from 'components/BigPhrase'
+import EditCardStatusButtons from 'components/edit-status-buttons'
+import SectionTranslations from 'components/translations-section'
+import TinyPhrase from 'components/TinyPhrase'
+import { usePhrase } from 'app/data/hooks'
+
+export default function Client({ pid }) {
+  const {
+    data: phrase,
+    error: phraseError,
+    status: phraseStatus,
+  } = usePhrase(pid)
+
+  return (
+    phrase && (
+      <main className="card-white">
+        <h2 lang={phrase.lang} className="h3 font-bold">
+          <TinyPhrase lang={phrase.lang} text={phrase.text} />
+        </h2>
+        <SectionTranslations
+          translations={phrase?.translations}
+          lang={phrase.lang}
+          phraseId={pid}
+          phraseText={phrase.text}
+        />
+        <SectionSeeAlsos
+          seeAlsos={phrase?.see_also_phrases}
+          linkFactory={(lang, pid) => `/my-decks/${lang}/phrase/${pid}`}
+        />
+        {phrase?.card ? (
+          <EditCardStatusButtons cardId={phrase?.card?.id} />
+        ) : (
+          <AddCardButtonsSection
+            phrase_id={pid}
+            user_deck_id={phrase?.card?.user_deck_id}
+            onClose={() => {}}
+          />
+        )}
+      </main>
+    )
+  )
+}
---
+++ src/app/(app)/review/[lang]/client.jsx
@@ -0,0 +1,94 @@
+'use client'
+
+import { useMemo, useState } from 'react'
+import languages from 'lib/languages'
+import Navbar from 'app/(app)/Navbar'
+import { useDeck } from 'app/data/hooks'
+import Loading from 'app/loading'
+import SuccessCheckmark from 'components/SvgComponents'
+import CardInner from './card'
+
+function shuffle(array) {
+  if (!array?.length > 0) return []
+  for (let currentIndex = array.length - 1; currentIndex > 0; currentIndex--) {
+    const randomIndex = Math.floor(Math.random() * (currentIndex + 1))
+    let temp = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temp
+  }
+  return array
+}
+
+const Empty = () => (
+  <p>
+    This is empty. You don&apos;t have any active cards. go choose some more
+    pls.
+  </p>
+)
+
+export default function ClientPage({ lang }) {
+  const { data, status } = useDeck(lang)
+  const reviewCards = useMemo(() => shuffle(data?.cards?.active), [data])
+  const [cardIndex, setCardIndex] = useState(0)
+  const [reviews, setReviews] = useState([])
+
+  if (status === 'loading') return <Loading />
+  if (!data?.cards?.active?.length > 0) return <Empty />
+
+  const canBackup = cardIndex > 0
+  const canAdvance = cardIndex < reviewCards.length - 1
+  const gobackaCard = () => setCardIndex(i => i - 1)
+  const advanceCard = () => setCardIndex(i => i + 1)
+  // This is different from advanceCard because it only wants to move forward
+  // by 1 after submitting a review, not to stack with button-clicks.
+  const nextCard = () => setCardIndex(cardIndex + 1)
+
+  const addReview = review => {
+    const index = reviews.findIndex(r => r.id === review.id)
+    let newReviews = reviews
+    if (index === -1) newReviews.push(review)
+    else newReviews[index] = review
+    setReviews(newReviews)
+  }
+
+  return (
+    <div className="h-full grid gap-8 pt-10 @lg:pt-0">
+      <Navbar
+        title={`Reviewing ${languages[lang]} (${cardIndex + 1} out of ${
+          reviewCards.length
+        })`}
+      />
+      <div className="flex justify-center gap-4 absolute @lg:static bottom-10 left-0 right-0">
+        <button
+          className="btn btn-primary"
+          onClick={gobackaCard}
+          disabled={!canBackup}
+        >
+          Prev card
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={advanceCard}
+          disabled={!canAdvance}
+        >
+          Next card
+        </button>
+      </div>
+      {cardIndex < reviewCards.length ? null : (
+        <div className="flex flex-row mx-auto gap-6 place-items-center my-10">
+          <SuccessCheckmark />
+          <p>All done for the day, nice work!</p>
+        </div>
+      )}
+      {reviewCards.map(c => (
+        <CardInner
+          key={c.id}
+          card={c}
+          nextCard={nextCard}
+          addReview={addReview}
+          hidden={c.id !== reviewCards[cardIndex]?.id}
+        />
+      ))}
+    </div>
+  )
+}
