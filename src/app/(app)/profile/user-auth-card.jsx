5f074fb9223d2b0e6d5e96300ebdd0a4c1556e43
Michael Snook <michael.snook@gmail.com>
2024-07-21 14:28:54 +0530

    Make the profile screen look nicer while loading

---
+++ src/app/(app)/profile/user-auth-card.jsx
@@ -0,0 +1,47 @@
+'use client'
+
+import Link from 'next/link'
+import { useAuth } from 'lib/auth-context'
+
+export default function UserAuthCard() {
+  const { userEmail } = useAuth()
+  return (
+    <div className="card card-body bg-base-100 text-base-content space-y-4">
+      <h2 className="card-title">Login credentials</h2>
+      <div className="flex flex-col w-full">
+        <label className="font-bold px-3">Your email</label>
+        <div className="flex flex-row gap-4">
+          <input
+            type="text"
+            className="border rounded p-3 flex-grow bg-base-300 text-base-content/70"
+            value={userEmail ?? 'loading...'}
+            disabled
+          />
+          <Link
+            href="/profile/change-email"
+            className="btn btn-link hover:bg-base-200"
+          >
+            Change
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <label className="font-bold px-3">Your password</label>
+        <div className="flex flex-row gap-4">
+          <input
+            type="text"
+            className="border rounded p-3 flex-grow bg-base-300 text-base-content/70"
+            value="***************"
+            disabled
+          />
+          <Link
+            href="/profile/change-password"
+            className="btn btn-link hover:bg-base-200"
+          >
+            Change
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
