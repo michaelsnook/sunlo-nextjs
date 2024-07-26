e34aebb79a52f71746ac709e5857a1e72fa9bd81
Em Snook <me@michaelsnook.com>
2024-07-26 23:32:20 +0530

    Update supabase types

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -3,10 +3,10 @@ export type Json =
   | number
   | boolean
   | null
-  | { [key: string]: Json }
+  | { [key: string]: Json | undefined }
   | Json[]
 
-export interface Database {
+export type Database = {
   public: {
     Tables: {
       language: {
@@ -30,18 +30,21 @@ export interface Database {
       phrase: {
         Row: {
           added_by: string | null
+          created_at: string | null
           id: string
           lang: string
           text: string
         }
         Insert: {
           added_by?: string | null
+          created_at?: string | null
           id?: string
           lang: string
           text: string
         }
         Update: {
           added_by?: string | null
+          created_at?: string | null
           id?: string
           lang?: string
           text?: string
@@ -50,15 +53,24 @@ export interface Database {
           {
             foreignKeyName: "phrase_added_by_fkey"
             columns: ["added_by"]
+            isOneToOne: false
             referencedRelation: "user_profile"
             referencedColumns: ["uid"]
           },
           {
             foreignKeyName: "phrase_lang_fkey"
             columns: ["lang"]
+            isOneToOne: false
             referencedRelation: "language"
             referencedColumns: ["lang"]
-          }
+          },
+          {
+            foreignKeyName: "phrase_lang_fkey"
+            columns: ["lang"]
+            isOneToOne: false
+            referencedRelation: "language_plus"
+            referencedColumns: ["lang"]
+          },
         ]
       }
       phrase_see_also: {
@@ -84,21 +96,24 @@ export interface Database {
           {
             foreignKeyName: "phrase_see_also_added_by_fkey"
             columns: ["added_by"]
+            isOneToOne: false
             referencedRelation: "user_profile"
             referencedColumns: ["uid"]
           },
           {
             foreignKeyName: "phrase_see_also_from_phrase_id_fkey"
             columns: ["from_phrase_id"]
+            isOneToOne: false
             referencedRelation: "phrase"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "phrase_see_also_to_phrase_id_fkey"
             columns: ["to_phrase_id"]
+            isOneToOne: false
             referencedRelation: "phrase"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       phrase_translation: {
@@ -130,25 +145,36 @@ export interface Database {
           {
             foreignKeyName: "phrase_translation_added_by_fkey"
             columns: ["added_by"]
+            isOneToOne: false
             referencedRelation: "user_profile"
             referencedColumns: ["uid"]
           },
           {
             foreignKeyName: "phrase_translation_lang_fkey"
             columns: ["lang"]
+            isOneToOne: false
             referencedRelation: "language"
+            referencedColumns: ["lang"]
+          },
+          {
+            foreignKeyName: "phrase_translation_lang_fkey"
+            columns: ["lang"]
+            isOneToOne: false
+            referencedRelation: "language_plus"
             referencedColumns: ["lang"]
           },
           {
             foreignKeyName: "phrase_translation_phrase_id_fkey"
             columns: ["phrase_id"]
+            isOneToOne: false
             referencedRelation: "phrase"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       user_card: {
         Row: {
+          created_at: string | null
           id: string
           phrase_id: string
           status: string
@@ -157,6 +183,7 @@ export interface Database {
           user_deck_id: string
         }
         Insert: {
+          created_at?: string | null
           id?: string
           phrase_id: string
           status?: string
@@ -165,6 +192,7 @@ export interface Database {
           user_deck_id: string
         }
         Update: {
+          created_at?: string | null
           id?: string
           phrase_id?: string
           status?: string
@@ -176,35 +204,97 @@ export interface Database {
           {
             foreignKeyName: "user_card_phrase_id_fkey"
             columns: ["phrase_id"]
+            isOneToOne: false
             referencedRelation: "phrase"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "user_card_uid_fkey"
             columns: ["uid"]
+            isOneToOne: false
             referencedRelation: "user_profile"
             referencedColumns: ["uid"]
           },
           {
             foreignKeyName: "user_card_user_deck_id_fkey"
             columns: ["user_deck_id"]
+            isOneToOne: false
             referencedRelation: "user_deck"
             referencedColumns: ["id"]
-          }
+          },
+          {
+            foreignKeyName: "user_card_user_deck_id_fkey"
+            columns: ["user_deck_id"]
+            isOneToOne: false
+            referencedRelation: "user_deck_mine"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "user_card_user_deck_id_fkey"
+            columns: ["user_deck_id"]
+            isOneToOne: false
+            referencedRelation: "user_deck_plus"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      user_card_review: {
+        Row: {
+          card_id: string
+          created_at: string
+          id: string
+          score: number | null
+          uid: string
+          updated_at: string
+        }
+        Insert: {
+          card_id?: string
+          created_at?: string
+          id?: string
+          score?: number | null
+          uid?: string
+          updated_at?: string
+        }
+        Update: {
+          card_id?: string
+          created_at?: string
+          id?: string
+          score?: number | null
+          uid?: string
+          updated_at?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "user_card_review_card_id_fkey"
+            columns: ["card_id"]
+            isOneToOne: false
+            referencedRelation: "user_card"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "user_card_review_uid_fkey"
+            columns: ["uid"]
+            isOneToOne: false
+            referencedRelation: "user_profile"
+            referencedColumns: ["uid"]
+          },
         ]
       }
       user_deck: {
         Row: {
+          created_at: string
           id: string
           lang: string
           uid: string
         }
         Insert: {
+          created_at?: string
           id?: string
           lang: string
           uid?: string
         }
         Update: {
+          created_at?: string
           id?: string
           lang?: string
           uid?: string
@@ -213,15 +303,24 @@ export interface Database {
           {
             foreignKeyName: "user_deck_lang_fkey"
             columns: ["lang"]
+            isOneToOne: false
             referencedRelation: "language"
+            referencedColumns: ["lang"]
+          },
+          {
+            foreignKeyName: "user_deck_lang_fkey"
+            columns: ["lang"]
+            isOneToOne: false
+            referencedRelation: "language_plus"
             referencedColumns: ["lang"]
           },
           {
             foreignKeyName: "user_deck_uid_fkey"
             columns: ["uid"]
+            isOneToOne: false
             referencedRelation: "user_profile"
             referencedColumns: ["uid"]
-          }
+          },
         ]
       }
       user_profile: {
@@ -256,174 +355,134 @@ export interface Database {
       }
     }
     Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      [_ in never]: never
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-  storage: {
-    Tables: {
-      buckets: {
+      language_plus: {
         Row: {
-          created_at: string | null
-          id: string
-          name: string
-          owner: string | null
-          public: boolean | null
-          updated_at: string | null
-        }
-        Insert: {
-          created_at?: string | null
-          id: string
-          name: string
-          owner?: string | null
-          public?: boolean | null
-          updated_at?: string | null
-        }
-        Update: {
-          created_at?: string | null
-          id?: string
-          name?: string
-          owner?: string | null
-          public?: boolean | null
-          updated_at?: string | null
-        }
-        Relationships: [
-          {
-            foreignKeyName: "buckets_owner_fkey"
-            columns: ["owner"]
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-      migrations: {
-        Row: {
-          executed_at: string | null
-          hash: string
-          id: number
-          name: string
-        }
-        Insert: {
-          executed_at?: string | null
-          hash: string
-          id: number
-          name: string
-        }
-        Update: {
-          executed_at?: string | null
-          hash?: string
-          id?: number
-          name?: string
+          alias_of: string | null
+          display_order: number | null
+          lang: string | null
+          learners: number | null
+          name: string | null
+          phrases_to_learn: number | null
+          rank: number | null
         }
         Relationships: []
       }
-      objects: {
+      phrase_relation: {
         Row: {
-          bucket_id: string | null
+          added_by: string | null
+          id: string | null
+          that_phrase: string | null
+          this_phrase: string | null
+        }
+        Relationships: []
+      }
+      user_card_review_plus: {
+        Row: {
+          card_id: string | null
           created_at: string | null
-          id: string
-          last_accessed_at: string | null
-          metadata: Json | null
-          name: string | null
-          owner: string | null
-          path_tokens: string[] | null
-          updated_at: string | null
-        }
-        Insert: {
-          bucket_id?: string | null
-          created_at?: string | null
-          id?: string
-          last_accessed_at?: string | null
-          metadata?: Json | null
-          name?: string | null
-          owner?: string | null
-          path_tokens?: string[] | null
-          updated_at?: string | null
-        }
-        Update: {
-          bucket_id?: string | null
-          created_at?: string | null
-          id?: string
-          last_accessed_at?: string | null
-          metadata?: Json | null
-          name?: string | null
-          owner?: string | null
-          path_tokens?: string[] | null
-          updated_at?: string | null
+          id: string | null
+          lang: string | null
+          score: number | null
         }
         Relationships: [
           {
-            foreignKeyName: "objects_bucketId_fkey"
-            columns: ["bucket_id"]
-            referencedRelation: "buckets"
+            foreignKeyName: "user_card_review_card_id_fkey"
+            columns: ["card_id"]
+            isOneToOne: false
+            referencedRelation: "user_card"
             referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "objects_owner_fkey"
-            columns: ["owner"]
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
+            foreignKeyName: "user_deck_lang_fkey"
+            columns: ["lang"]
+            isOneToOne: false
+            referencedRelation: "language"
+            referencedColumns: ["lang"]
+          },
+          {
+            foreignKeyName: "user_deck_lang_fkey"
+            columns: ["lang"]
+            isOneToOne: false
+            referencedRelation: "language_plus"
+            referencedColumns: ["lang"]
+          },
+        ]
+      }
+      user_deck_mine: {
+        Row: {
+          cards_active: number | null
+          cards_learned: number | null
+          cards_skipped: number | null
+          created_at: string | null
+          id: string | null
+          lang: string | null
+          lang_total_phrases: number | null
+          most_recent_review_at: string | null
+          uid: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "user_deck_lang_fkey"
+            columns: ["lang"]
+            isOneToOne: false
+            referencedRelation: "language"
+            referencedColumns: ["lang"]
+          },
+          {
+            foreignKeyName: "user_deck_lang_fkey"
+            columns: ["lang"]
+            isOneToOne: false
+            referencedRelation: "language_plus"
+            referencedColumns: ["lang"]
+          },
+          {
+            foreignKeyName: "user_deck_uid_fkey"
+            columns: ["uid"]
+            isOneToOne: false
+            referencedRelation: "user_profile"
+            referencedColumns: ["uid"]
+          },
+        ]
+      }
+      user_deck_plus: {
+        Row: {
+          cards_active: number | null
+          cards_learned: number | null
+          cards_skipped: number | null
+          created_at: string | null
+          id: string | null
+          lang: string | null
+          lang_total_phrases: number | null
+          most_recent_review_at: string | null
+          uid: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "user_deck_lang_fkey"
+            columns: ["lang"]
+            isOneToOne: false
+            referencedRelation: "language"
+            referencedColumns: ["lang"]
+          },
+          {
+            foreignKeyName: "user_deck_lang_fkey"
+            columns: ["lang"]
+            isOneToOne: false
+            referencedRelation: "language_plus"
+            referencedColumns: ["lang"]
+          },
+          {
+            foreignKeyName: "user_deck_uid_fkey"
+            columns: ["uid"]
+            isOneToOne: false
+            referencedRelation: "user_profile"
+            referencedColumns: ["uid"]
+          },
         ]
       }
     }
-    Views: {
-      [_ in never]: never
-    }
     Functions: {
-      extension: {
-        Args: {
-          name: string
-        }
-        Returns: string
-      }
-      filename: {
-        Args: {
-          name: string
-        }
-        Returns: string
-      }
-      foldername: {
-        Args: {
-          name: string
-        }
-        Returns: unknown
-      }
-      get_size_by_bucket: {
-        Args: Record<PropertyKey, never>
-        Returns: {
-          size: number
-          bucket_id: string
-        }[]
-      }
-      search: {
-        Args: {
-          prefix: string
-          bucketname: string
-          limits?: number
-          levels?: number
-          offsets?: number
-          search?: string
-          sortcolumn?: string
-          sortorder?: string
-        }
-        Returns: {
-          name: string
-          id: string
-          updated_at: string
-          created_at: string
-          last_accessed_at: string
-          metadata: Json
-        }[]
-      }
+      [_ in never]: never
     }
     Enums: {
       [_ in never]: never
@@ -433,3 +492,85 @@ export interface Database {
     }
   }
 }
+
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
