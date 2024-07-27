ecbe6df8209a2b4215dae056996154a5bfaf25cc
Em Snook <me@michaelsnook.com>
2024-07-27 16:42:01 +0530

    Shore up the Error component, and comment it

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -1,13 +1,21 @@
-import { ReactElement } from 'react'
+import type { PropsWithChildren } from 'react'
+
+/*
+  If the error message passed as `children` is nullable, we can simply use:
+
+    <Error>{some nullable message}</Error>
+    
+  But when we want to put some text directly in the template, like `Error: ${message}` it will mean
+  that `children` is never null, so we add the `show` prop:
+
+    <Error show={error !== null}>Error submitting form: {error.message}</Error>
+*/
 
 export default function Error({
-  message,
-  children,
-}: {
-  message: string
-  children: ReactElement | string
-}) {
-  return !message ? null : (
+  show = null,
+  children = null,
+}: PropsWithChildren<{ show?: boolean | null }>) {
+  return show === false || (show === null && children === null) ? null : (
     <div className="alert alert-error">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -22,10 +30,7 @@ export default function Error({
           d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
-      <div>
-        {children}
-        {message}
-      </div>
+      <div>{children || `An unknown error has occurred (sorry)`}</div>
     </div>
   )
 }
