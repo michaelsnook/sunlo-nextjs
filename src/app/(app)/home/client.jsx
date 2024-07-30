7c59c1e6eb0e0f277a87692b4190f4e6c2f953ba
Michael Snook <michael.snook@gmail.com>
2024-07-31 01:04:02 +0530

    Sort on the server instead

--- src/app/(app)/home/client.jsx
+++ src/app/(app)/home/client.jsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useMemo } from 'react'
 import Link from 'next/link'
 import { useProfile, useRecentReviewActivity } from 'app/data/hooks'
 import languages from 'lib/languages'
@@ -10,35 +9,15 @@ export default function Client() {
   const { data: profile, isLoading: isLoadingProfile } = useProfile()
   const { data: reviews, isLoading: isLoadingReviews } =
     useRecentReviewActivity()
-  // console.log(`This is the profile and reviews objects`, profile, reviews)
-
-  const [activeDecks, remainingDecks] = useMemo(() => {
-    if (isLoadingProfile || isLoadingReviews) return [[], []]
-    const actives = reviews?.keysInOrder ?? []
-    return [
-      actives,
-      profile.deck_stubs
-        .filter(d => !actives?.includes(d.lang) ?? false)
-        .sort((left, right) => {
-          // the deck that was updated more recently sorts first
-          return left?.updated_at === right?.updated_at
-            ? 0
-            : left?.updated_at > right?.updated_at
-              ? -1
-              : 1
-        })
-        .map(d => d.lang),
-    ]
-  }, [profile, reviews, isLoadingProfile, isLoadingReviews])
 
   return isLoadingProfile || isLoadingReviews ? (
     <Loading />
   ) : (
     <ol>
-      {[...activeDecks, ...remainingDecks]?.map(lang => (
-        <li key={lang} className="glass my-2 rounded p-2 text-center">
-          <Link href={`home/${lang}`}>
-            <p className="py-2 text-xl">{languages[lang]}</p>
+      {profile.deck_stubs?.map(deck => (
+        <li key={deck.lang} className="glass my-2 rounded p-2 text-center">
+          <Link href={`home/${deck.lang}`}>
+            <p className="py-2 text-xl">{languages[deck.lang]}</p>
           </Link>
         </li>
       ))}
