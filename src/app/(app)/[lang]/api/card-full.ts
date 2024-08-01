fee586b61cc24d9855c2bcfdeb06f9f6815240e5
Michael Snook <michael.snook@gmail.com>
2024-08-01 14:59:09 +0530

    Add deck mutation and keep trying to nip type issues

--- src/app/(app)/[lang]/api/card-full.ts
+++ src/app/(app)/[lang]/api/card-full.ts
@@ -1,17 +1,16 @@
 import supabase from 'lib/supabase-client'
 import { useQuery } from '@tanstack/react-query'
+import type { uuid, UseSBQuery, CardFull } from 'types/main'
 
 export function useCard(lang: string, pid: uuid): UseSBQuery<CardFull> {
   return useQuery({
     queryKey: ['deck', lang, 'card', pid],
     queryFn: async ({ queryKey }) => {
       let { data, error } = await supabase
-        .from('user_card')
+        .from('user_card_plus')
         .select('*, reviews:user_card_review(*)')
         .eq('phrase_id', queryKey[3])
       if (error) throw error
-      // do we want to make sure it exists in the pids array?
-      data.reviews ??= []
       return data
     },
   })
--- src/app/(app)/[lang]/api/deck.ts
+++ src/app/(app)/[lang]/api/deck.ts
@@ -1,13 +1,24 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import supabase from 'lib/supabase-client'
+import { DeckInsert, DeckMeta } from 'types/main'
 // postNewDeck
-async function postInsertDeck() {}
-function cacheDeck(data): void {}
+async function postInsertDeck(values: DeckInsert) {
+  const { data, error } = await supabase.from('user_deck').insert(values)
+  if (error) throw error
+  return data
+}
+
 // createNewDeck
 function useInsertDeck() {
+  const client = useQueryClient()
   return useMutation({
-    mutationKey: [],
-    mutationFn: () => {},
-    onSuccess: cacheNewDeck,
+    // mutationKey: [],
+    mutationFn: async (values: DeckInsert) => postInsertDeck(values),
+    onSuccess: (data, _error, values: DeckInsert) => {
+      // do not optimistically update because we need the user_deck_plus view
+      client.invalidateQueries({ queryKey: ['user_profile'] })
+      client.invalidateQueries({ queryKey: ['deck', values.lang, 'meta'] })
+    },
   })
 }
 // update deck: nope
