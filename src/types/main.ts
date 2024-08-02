896cbfd53dbe0f72616b2245721f2058e93f3309
Michael Snook <michael.snook@gmail.com>
2024-08-03 04:34:11 +0530

    Use Supabase Table type aliases

--- src/types/main.ts
+++ src/types/main.ts
@@ -1,4 +1,4 @@
-import { Database } from './supabase'
+import { Database, Tables } from './supabase'
 import { UseMutationResult, UseQueryResult } from '@tanstack/react-query'
 import {
   PostgrestError,
@@ -16,28 +16,24 @@ export type SBMutation<T> = Promise<PostgrestResponse<T>>
 // type Tables = Database['public']['Tables']
 // type Views = Database['public']['Views']
 
-export type LanguageRow = Omit<
-  Database['public']['Tables']['language']['Row'],
-  'alias_of'
->
-export type LanguageMeta = Database['public']['Views']['language_plus']['Row']
+export type LanguageRow = Omit<Tables<'language'>, 'alias_of'>
+export type LanguageMeta = Tables<'language_plus'>
 export type LanguageFull = LanguageMeta & {
   phrases: Array<PhraseFull>
 }
 
-export type PhraseRow = Database['public']['Tables']['phrase']['Row']
+export type PhraseRow = Tables<'phrase'>
 export type PhraseInsert = Database['public']['Tables']['phrase']['Insert']
 
-export type TranslationRow =
-  Database['public']['Tables']['phrase_translation']['Row']
+export type TranslationRow = Tables<'phrase_translation'>
 export type TranslationInsert =
   Database['public']['Tables']['phrase_translation']['Insert']
 
-export type RelationRow = Database['public']['Tables']['phrase_relation']['Row']
+export type RelationRow = Tables<'phrase_relation'>
 export type RelationInsert =
   Database['public']['Tables']['phrase_relation']['Insert']
 
-export type PhraseMeta = Views['phrase_plus']
+export type PhraseMeta = Tables<'phrase_plus'>
 export type NewPhraseFull = PhraseMeta & {
   translations: Array<TranslationRow>
 }
@@ -51,19 +47,32 @@ export type PhraseFullInsert = PhraseInsert & {
   // card?: CardInsert
 }
 
-export type DeckRow = Database['public']['Tables']['user_deck']['Row']
+export type DeckRow = Tables<'user_deck'>
 export type DeckInsert = Database['public']['Tables']['user_deck']['Insert']
-export type DeckMeta = Database['public']['Views']['user_deck_plus']['Row']
+export type DeckMeta = Tables<'user_deck_plus'>
 export type DeckFull = DeckMeta & {
-  cards?: Array<CardFull>
+  cards: Array<CardFull>
 }
 
-export type CardRow = Database['public']['Tables']['user_card']['Row']
-export type CardMeta = Database['public']['Views']['user_card_plus']['Row']
+type C = {
+  cards_active: number
+  cards_learned: number
+  cards_skipped: number
+  created_at: string
+  id: string
+  lang: string
+  lang_total_phrases: number
+  most_recent_review_at: string
+  uid: string
+  cards: CardFull[]
+} // ' is not assignable to parameter of type 'DeckFull'
+
+export type CardRow = Tables<'user_card'>
+export type CardMeta = Tables<'user_card_plus'>
 export type CardInsert = Database['public']['Tables']['user_card']['Insert']
 
-export type ReviewMeta = Database['public']['Views']['user_card_review_plus']
-export type ReviewRow = Database['public']['Tables']['user_card_review']['Row']
+export type ReviewMeta = Tables<'user_card_review_plus'>
+export type ReviewRow = Tables<'user_card_review'>
 export type ReviewInsert =
   Database['public']['Tables']['user_card_review']['Insert']
 
@@ -71,21 +80,20 @@ export type CardFull = CardMeta & {
   reviews?: Array<ReviewMeta>
 }
 
-// export type ProfilePublic = Database['public']['Views']['public_profile']['Row']
-export type ProfileRow = Database['public']['Tables']['user_profile']['Row']
-export type ProfileMeta = ProfileRow // Database['public']['Views']['profile_meta']['Row']
+// export type ProfilePublic = Tables<'public_profile'>
+export type ProfileRow = Tables<'user_profile'>
+export type ProfileMeta = ProfileRow // Tables<'profile_meta'>
 export type ProfileFull = ProfileMeta & {
   decks: Array<DeckMeta>
 }
 
 export type Profile = ProfileRow & { deck_stubs: Array<DeckMeta> }
-export type PhraseStub = Database['public']['Tables']['phrase']['Row']
-export type Translation =
-  Database['public']['Tables']['phrase_translation']['Row']
-export type DeckStub = Database['public']['Tables']['user_deck']['Row']
-export type CardStub = Database['public']['Tables']['user_card']['Row']
+export type PhraseStub = Tables<'phrase'>
+export type Translation = Tables<'phrase_translation'>
+export type DeckStub = Tables<'user_deck'>
+export type CardStub = Tables<'user_card'>
 export type UserCardInsert = Database['public']['Tables']['user_card']['Insert']
-export type Review = Database['public']['Tables']['user_card_review']
+export type Review = Tables<'user_card_review'>
 
 // for legacy hooks and such
 
