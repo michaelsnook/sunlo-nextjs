c197329d02c6306a2dccf4b679d93deb50832ea1
Michael Snook <michael.snook@gmail.com>
2024-08-05 17:13:24 +0530

    Combine fetching+shaping, so they always happen together

--- src/app/(app)/[lang]/api/preload-language.ts
+++ src/app/(app)/[lang]/api/preload-language.ts
@@ -1,28 +1,22 @@
-import { QueryKey, UseQueryResult, useQuery } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import type {
+  PhrasesMap,
   LanguageFetched,
   LanguageLoaded,
-  PhrasesMap,
   pids,
 } from 'types/main'
 import { mapArray, selects } from 'lib/utils'
 import supabase from 'lib/supabase-client'
+import { useLang } from '../app-data-provider'
 
-// this fetcher can be called on the server
-export async function fetchLanguage(lang: string): Promise<LanguageFetched> {
+export async function fetchLanguage(lang: string): Promise<LanguageLoaded> {
   const { data } = await supabase
     .from('language_plus')
     .select(selects.language_full())
     .eq('lang', lang)
     .maybeSingle()
     .throwOnError()
-  return data
-}
-
-function transformLanguageFetchedToLoaded({
-  phrases: phrasesArray = [],
-  ...meta
-}: LanguageFetched): LanguageLoaded {
+  const { phrases: phrasesArray, ...meta }: LanguageFetched = data
   const pids: pids = phrasesArray?.map(p => p.id)
   const phrases: PhrasesMap = mapArray(phrasesArray, 'id')
   return {
@@ -33,26 +27,20 @@ function transformLanguageFetchedToLoaded({
 }
 
 export function useLanguageQuery(
-  lang: string,
-  select = null
-): UseQueryResult<LanguageLoaded> {
-  return useQuery<LanguageLoaded>({
+  select: undefined | any,
+  { lang: altLang }: { lang?: string }
+) {
+  const paramLang = useLang()
+  const lang = altLang || paramLang
+  return useQuery({
     queryKey: ['language', lang, 'loaded'],
-    queryFn: async ({
-      queryKey,
-    }: {
-      queryKey: QueryKey
-    }): Promise<LanguageFetched | null | any> => {
-      const lang = queryKey[1] as string
-      const data: LanguageFetched = await fetchLanguage(lang)
-      const result: LanguageLoaded = transformLanguageFetchedToLoaded(data)
-      return result
+    queryFn: () => {
+      return fetchLanguage(lang)
     },
     select,
     enabled: typeof lang === 'string' && lang.length === 3,
-    gcTime: Infinity,
-    staleTime: Infinity,
-    refetchOnMount: false,
+    gcTime: 120_000,
+    staleTime: 120_000,
     refetchOnWindowFocus: false,
   })
 }
